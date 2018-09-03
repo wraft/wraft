b4b9fdd0443a43949a150c5b6f92060b6792ddb0
shijith <shijith.k@aurut.com>
2018-09-03 23:48:41 +0530

    chore: added fallback error controls for user login

--- lib/starter_web/controllers/fallback_controller.ex
+++ lib/starter_web/controllers/fallback_controller.ex
@@ -11,7 +11,17 @@ defmodule StarterWeb.FallbackController do
     |> put_status(:unprocessable_entity)
     |> render(StarterWeb.ChangesetView, "error.json", changeset: changeset)
   end
+  
+  def call(conn, {:error, :invalid}) do
+    body = Poison.encode!(%{error: "Your email-password combination doesn't match. Please try again.!"})
+    send_resp(conn, 404, body)
+  end
 
+  def call(conn, {:error, :no_data}) do
+    body = Poison.encode!(%{error: "Please provide all necessary datas to login.!"})
+    send_resp(conn, 400, body)
+  end
+  
   def call(conn, nil) do
     conn
     |> put_status(:not_found)
