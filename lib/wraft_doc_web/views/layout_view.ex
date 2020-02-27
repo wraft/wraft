838037cc383f35cb22080a416ca10ae75f0dbd1f
Shijith K <shijith.k@aurut.com>
2020-02-27 13:12:57 +0530

    feat: create content type

--- lib/wraft_doc_web/views/layout_view.ex
+++ lib/wraft_doc_web/views/layout_view.ex
@@ -16,4 +16,18 @@ defmodule WraftDocWeb.Api.V1.LayoutView do
       engine: render_one(layout.engine, EngineView, "create.json", as: :engine)
     }
   end
+
+  def render("preloaded_layout.json", %{doc_layout: layout}) do
+    %{
+      uuid: layout.uuid,
+      name: layout.name,
+      description: layout.description,
+      width: layout.width,
+      height: layout.height,
+      unit: layout.unit,
+      slug: layout.slug,
+      inserted_at: layout.inserted_at,
+      update_at: layout.updated_at
+    }
+  end
 end
