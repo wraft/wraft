a33c22cdd2e832af223b102516b1ca674f51cc26
Shijith K <shijithjayan@gmail.com>
2020-03-06 13:01:19 +0530

    feat: instance index

--- lib/wraft_doc_web/views/instance_view.ex
+++ lib/wraft_doc_web/views/instance_view.ex
@@ -1,6 +1,7 @@
 defmodule WraftDocWeb.Api.V1.InstanceView do
   use WraftDocWeb, :view
   alias WraftDocWeb.Api.V1.{ContentTypeView, StateView}
+  alias __MODULE__
 
   def render("create.json", %{content: content}) do
     %{
@@ -16,5 +17,20 @@ defmodule WraftDocWeb.Api.V1.InstanceView do
         render_one(content.content_type, ContentTypeView, "content_type.json", as: :content_type),
       state: render_one(content.state, StateView, "create.json", as: :state)
     }
+  end
+
+  def render("instance.json", %{instance: instance}) do
+    %{
+      id: instance.uuid,
+      instance_id: instance.instance_id,
+      raw: instance.raw,
+      serialized: instance.serialized,
+      inserted_at: instance.inserted_at,
+      updated_at: instance.updated_at
+    }
+  end
+
+  def render("index.json", %{contents: contents}) do
+    render_many(contents, InstanceView, "instance.json", as: :instance)
   end
 end
