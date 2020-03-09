0d8ee81d2055533742b255c3b2c976ffda3c11e4
Shijith K <shijith.k@aurut.com>
2020-03-09 15:43:42 +0530

    feat: create asset

--- lib/wraft_doc_web/views/asset_view.ex
+++ lib/wraft_doc_web/views/asset_view.ex
@@ -1,3 +1,12 @@
 defmodule WraftDocWeb.Api.V1.AssetView do
   use WraftDocWeb, :view
+
+  def render("asset.json", %{asset: asset}) do
+    %{
+      id: asset.uuid,
+      name: asset.name,
+      inserted_at: asset.inserted_at,
+      updated_at: asset.updated_at
+    }
+  end
 end
