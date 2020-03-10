dd5ba456f669d25bac7a93b328241205acab0a1b
Shijith K <shijithjayan@gmail.com>
2020-03-10 17:42:35 +0530

    refactor: added color to content type

--- lib/wraft_doc_web/views/content_type_view.ex
+++ lib/wraft_doc_web/views/content_type_view.ex
@@ -10,6 +10,7 @@ defmodule WraftDocWeb.Api.V1.ContentTypeView do
       name: c_type.name,
       decription: c_type.description,
       fields: c_type.fields,
+      color: c_type.color,
       inserted_at: c_type.inserted_at,
       updated_at: c_type.updated_at,
       layout: render_one(c_type.layout, LayoutView, "layout.json", as: :doc_layout)
@@ -33,6 +34,7 @@ defmodule WraftDocWeb.Api.V1.ContentTypeView do
       name: c_type.name,
       decription: c_type.description,
       fields: c_type.fields,
+      color: c_type.color,
       inserted_at: c_type.inserted_at,
       updated_at: c_type.updated_at
     }
