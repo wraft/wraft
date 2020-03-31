e92b8f41254272cf67e1f256c42eccd45390c769
Shijith K <shijith.k@aurut.com>
2020-03-31 13:44:11 +0530

    feat: create field type

---
+++ lib/wraft_doc/document/field_type.ex
@@ -0,0 +1,24 @@
+defmodule WraftDoc.Document.FieldType do
+  @moduledoc """
+    The field type model.
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "field_type" do
+    field(:uuid, Ecto.UUID, autogenerate: true, null: false)
+    field(:name, :string, null: false)
+    belongs_to(:creator, WraftDoc.Account.User)
+    timestamps()
+  end
+
+  def changeset(field_type, attrs \\ %{}) do
+    field_type
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> unique_constraint(:name,
+      message: "Field type with the same name exists. Use another name.!",
+      name: :field_type_unique_index
+    )
+  end
+end
---
+++ lib/wraft_doc_web/controllers/field_type_controller.ex
@@ -0,0 +1,215 @@
+defmodule WraftDocWeb.Api.V1.FieldTypeController do
+  use WraftDocWeb, :controller
+  use PhoenixSwagger
+  action_fallback(WraftDocWeb.FallbackController)
+  alias WraftDoc.{Document, Document.FieldType}
+
+  def swagger_definitions do
+    %{
+      FieldTypeRequest:
+        swagger_schema do
+          title("Field type request")
+          description("Field type request")
+
+          properties do
+            name(:string, "Name of the asset")
+          end
+
+          example(%{
+            name: "Date"
+          })
+        end,
+      FieldType:
+        swagger_schema do
+          title("Field type")
+          description("A field type.")
+
+          properties do
+            id(:string, "The ID of the asset", required: true)
+            name(:string, "Name of the asset")
+            inserted_at(:string, "When was the engine inserted", format: "ISO-8601")
+            updated_at(:string, "When was the engine last updated", format: "ISO-8601")
+          end
+
+          example(%{
+            id: "1232148nb3478",
+            name: "Date",
+            updated_at: "2020-01-21T14:00:00Z",
+            inserted_at: "2020-02-21T14:00:00Z"
+          })
+        end,
+      FieldTypes:
+        swagger_schema do
+          title("All field types")
+          description("All filed types that have been created so far")
+          type(:array)
+          items(Schema.ref(:FieldType))
+        end,
+      FieldTypeIndex:
+        swagger_schema do
+          properties do
+            assets(Schema.ref(:FieldTypes))
+            page_number(:integer, "Page number")
+            total_pages(:integer, "Total number of pages")
+            total_entries(:integer, "Total number of contents")
+          end
+
+          example(%{
+            field_types: [
+              %{
+                id: "1232148nb3478",
+                name: "Date",
+                updated_at: "2020-01-21T14:00:00Z",
+                inserted_at: "2020-02-21T14:00:00Z"
+              }
+            ],
+            page_number: 1,
+            total_pages: 2,
+            total_entries: 15
+          })
+        end
+    }
+  end
+
+  @doc """
+  Create a field type.
+  """
+  swagger_path :create do
+    post("/field_types")
+    summary("Create a field type")
+    description("Create field type API")
+    operation_id("create_field_type")
+
+    parameters do
+      field_type(:body, Schema.ref(:FieldTypeRequest), "Field Type to be created", required: true)
+    end
+
+    response(200, "Ok", Schema.ref(:FieldType))
+    response(422, "Unprocessable Entity", Schema.ref(:Error))
+    response(401, "Unauthorized", Schema.ref(:Error))
+  end
+
+  @spec create(Plug.Conn.t(), map) :: Plug.Conn.t()
+  def create(conn, params) do
+    current_user = conn.assigns[:current_user]
+
+    with {:ok, %FieldType{} = field_type} <- Document.create_field_type(current_user, params) do
+      conn
+      |> render(:field_type, field_type: field_type)
+    end
+  end
+
+  # @doc """
+  # Asset index.
+  # """
+  # swagger_path :index do
+  #   get("/assets")
+  #   summary("Asset index")
+  #   description("API to get the list of all assets created so far under an organisation")
+
+  #   parameter(:page, :query, :string, "Page number")
+
+  #   response(200, "Ok", Schema.ref(:AssetsIndex))
+  #   response(401, "Unauthorized", Schema.ref(:Error))
+  # end
+
+  # @spec index(Plug.Conn.t(), map) :: Plug.Conn.t()
+  # def index(conn, params) do
+  #   %{organisation_id: org_id} = conn.assigns[:current_user]
+
+  #   with %{
+  #          entries: assets,
+  #          page_number: page_number,
+  #          total_pages: total_pages,
+  #          total_entries: total_entries
+  #        } <- Document.asset_index(org_id, params) do
+  #     conn
+  #     |> render("index.json",
+  #       assets: assets,
+  #       page_number: page_number,
+  #       total_pages: total_pages,
+  #       total_entries: total_entries
+  #     )
+  #   end
+  # end
+
+  # @doc """
+  # Show instance.
+  # """
+  # swagger_path :show do
+  #   get("/assets/{id}")
+  #   summary("Show an asset")
+  #   description("API to get all details of an asset")
+
+  #   parameters do
+  #     id(:path, :string, "ID of the asset", required: true)
+  #   end
+
+  #   response(200, "Ok", Schema.ref(:ShowAsset))
+  #   response(401, "Unauthorized", Schema.ref(:Error))
+  # end
+
+  # @spec show(Plug.Conn.t(), map) :: Plug.Conn.t()
+  # def show(conn, %{"id" => asset_uuid}) do
+  #   with %Asset{} = asset <- Document.show_asset(asset_uuid) do
+  #     conn
+  #     |> render("show.json", asset: asset)
+  #   end
+  # end
+
+  # @doc """
+  # Update an asset.
+  # """
+  # swagger_path :update do
+  #   put("/assets/{id}")
+  #   summary("Update an asset")
+  #   description("API to update an asset")
+
+  #   consumes("multipart/form-data")
+
+  #   parameter(:id, :path, :string, "asset id", required: true)
+  #   parameter(:name, :formData, :string, "Asset name", required: true)
+  #   parameter(:file, :formData, :file, "Asset file to upload")
+
+  #   response(200, "Ok", Schema.ref(:Asset))
+  #   response(422, "Unprocessable Entity", Schema.ref(:Error))
+  #   response(401, "Unauthorized", Schema.ref(:Error))
+  #   response(404, "Not found", Schema.ref(:Error))
+  # end
+
+  # @spec update(Plug.Conn.t(), map) :: Plug.Conn.t()
+  # def update(conn, %{"id" => uuid} = params) do
+  #   with %Asset{} = asset <- Document.get_asset(uuid),
+  #        {:ok, asset} <- Document.update_asset(asset, params) do
+  #     conn
+  #     |> render("asset.json", asset: asset)
+  #   end
+  # end
+
+  # @doc """
+  # Delete an asset.
+  # """
+  # swagger_path :delete do
+  #   PhoenixSwagger.Path.delete("/assets/{id}")
+  #   summary("Delete an asset")
+  #   description("API to delete an asset")
+
+  #   parameters do
+  #     id(:path, :string, "asset id", required: true)
+  #   end
+
+  #   response(200, "Ok", Schema.ref(:Asset))
+  #   response(422, "Unprocessable Entity", Schema.ref(:Error))
+  #   response(401, "Unauthorized", Schema.ref(:Error))
+  #   response(404, "Not found", Schema.ref(:Error))
+  # end
+
+  # @spec delete(Plug.Conn.t(), map) :: Plug.Conn.t()
+  # def delete(conn, %{"id" => uuid}) do
+  #   with %Asset{} = asset <- Document.get_asset(uuid),
+  #        {:ok, %Asset{}} <- Document.delete_asset(asset) do
+  #     conn
+  #     |> render("asset.json", asset: asset)
+  #   end
+  # end
+end
