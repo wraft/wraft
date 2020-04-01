4b3299cf9340da25644b00bfe96214569933165c
Shijith K <shijithjayan@gmail.com>
2020-04-01 20:08:19 +0530

    feat: activity stream for asset

--- lib/wraft_doc/document/asset.ex
+++ lib/wraft_doc/document/asset.ex
@@ -3,9 +3,21 @@ defmodule WraftDoc.Document.Asset do
     The asset model.
   """
   alias __MODULE__
+  alias WraftDoc.Account.User
   use Ecto.Schema
   use Arc.Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
+
+  defimpl Spur.Trackable, for: Asset do
+    def actor(asset), do: "#{asset.creator_id}"
+    def object(asset), do: "Asset:#{asset.id}"
+    def target(_chore), do: nil
+
+    def audience(%{organisation_id: id}) do
+      from(u in User, where: u.organisation_id == ^id)
+    end
+  end
 
   schema "asset" do
     field(:uuid, Ecto.UUID, autogenerate: true, null: false)
