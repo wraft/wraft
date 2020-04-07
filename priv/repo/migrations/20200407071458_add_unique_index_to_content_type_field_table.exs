f2626bbafaf730f198cdb613cdd204cad8c0d702
Shijith K <shijith.k@aurut.com>
2020-04-07 12:48:56 +0530

    fix: fields of a content type duplicating on content type update

---
+++ priv/repo/migrations/20200407071458_add_unique_index_to_content_type_field_table.exs
@@ -0,0 +1,11 @@
+defmodule WraftDoc.Repo.Migrations.AddUniqueIndexToContentTypeFieldTable do
+  use Ecto.Migration
+
+  def change do
+    create(
+      unique_index(:content_type_field, [:content_type_id, :field_type_id],
+        name: :content_type_field_unique_index
+      )
+    )
+  end
+end
