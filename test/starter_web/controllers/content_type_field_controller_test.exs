dcce990114a604671da76305fadc6522b41cf8ad
Shijith K <shijithjayan@gmail.com>
2020-04-21 11:11:10 +0530

    test: content type field controller test

---
+++ test/starter_web/controllers/content_type_field_controller_test.exs
@@ -0,0 +1,42 @@
+defmodule WraftDocWeb.ContentTypeFieldControllerTest do
+  @moduledoc """
+  Test module for content type field controller.
+  """
+  use WraftDocWeb.ConnCase
+  import WraftDoc.Factory
+  alias WraftDoc.{Repo, Document, Document.ContentTypeField}
+
+  setup %{conn: conn} do
+    user = insert(:user)
+
+    conn =
+      conn
+      |> put_req_header("accept", "application/json")
+      |> post(
+        Routes.v1_user_path(conn, :signin, %{
+          email: user.email,
+          password: user.password
+        })
+      )
+
+    conn = assign(conn, :current_user, user)
+
+    {:ok, %{conn: conn}}
+  end
+
+  test "delete content type field by given id", %{conn: conn} do
+    conn =
+      build_conn()
+      |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
+      |> assign(:current_user, conn.assigns.current_user)
+
+    content_type_field = insert(:content_type_field) |> Repo.preload(:content_type)
+    count_before = ContentTypeField |> Repo.all() |> length()
+
+    conn = delete(conn, Routes.v1_content_type_field_path(conn, :delete, content_type_field.uuid))
+    assert count_before - 1 == ContentTypeField |> Repo.all() |> length()
+
+    assert json_response(conn, 200)["content_type"]["name"] ==
+             content_type_field.content_type.name
+  end
+end
