ca8953c42c77850a754569c8e9fa2746c0730585
Shijith K <shijith.k@aurut.com>
2020-04-21 20:49:06 +0530

    test: instance version test

---
+++ test/starter/instance_version_test.exs
@@ -0,0 +1,24 @@
+defmodule WraftDoc.InstanceVersionTest do
+  use WraftDoc.ModelCase
+  import WraftDoc.Factory
+  alias WraftDoc.Document.Instance.Version
+  import Ecto
+
+  @valid_attrs %{
+    version_number: 1,
+    raw: "sample raw data",
+    serialized: %{body: "sample raw data"}
+  }
+  @invalid_attrs %{version_number: "v1"}
+
+  test "changeset with valid attributes" do
+    changeset = Version.changeset(%Version{}, @valid_attrs)
+    IO.inspect(changeset)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = Version.changeset(%Version{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
