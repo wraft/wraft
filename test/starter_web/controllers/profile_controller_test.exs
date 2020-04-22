30cc419cb0ef1a69a7deb27a74f3e72197532128
Shijith K <shijith.k@aurut.com>
2020-04-22 10:22:14 +0530

    test: updated tests in profile controller

--- test/starter_web/controllers/profile_controller_test.exs
+++ test/starter_web/controllers/profile_controller_test.exs
@@ -40,31 +40,31 @@ defmodule WraftDocWeb.ProfileControllerTest do
       |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
       |> assign(:current_user, conn.assigns.current_user)
 
-    conn = put(conn, Routes.v1_profile_path(conn, :update, profile.id), params)
+    conn = put(conn, Routes.v1_profile_path(conn, :update), params)
     assert json_response(conn, 200)["name"] == @valid_attrs.name
   end
 
-  test "render profile on id exists", %{conn: conn} do
-    profile = insert(:profile)
+  # test "render profile on id exists", %{conn: conn} do
+  #   profile = insert(:profile)
 
-    conn =
-      build_conn()
-      |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
-      |> assign(:current_user, conn.assigns.current_user)
+  #   conn =
+  #     build_conn()
+  #     |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
+  #     |> assign(:current_user, conn.assigns.current_user)
 
-    conn = get(conn, Routes.v1_profile_path(conn, :show, profile.uuid))
-    assert json_response(conn, 200)["name"] == profile.name
-  end
+  #   conn = get(conn, Routes.v1_profile_path(conn, :show, profile.uuid))
+  #   assert json_response(conn, 200)["name"] == profile.name
+  # end
 
-  test "error not found for id does not exist", %{conn: conn} do
-    conn =
-      build_conn()
-      |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
-      |> assign(:current_user, conn.assigns.current_user)
+  # test "error not found for id does not exist", %{conn: conn} do
+  #   conn =
+  #     build_conn()
+  #     |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
+  #     |> assign(:current_user, conn.assigns.current_user)
 
-    conn = get(conn, Routes.v1_profile_path(conn, :show, Ecto.UUID.autogenerate()))
-    assert json_response(conn, 404) == "Not Found"
-  end
+  #   conn = get(conn, Routes.v1_profile_path(conn, :show, Ecto.UUID.autogenerate()))
+  #   assert json_response(conn, 404) == "Not Found"
+  # end
 
   test "renders current profile ", %{conn: conn} do
     current_user = conn.assigns.current_user
@@ -77,21 +77,5 @@ defmodule WraftDocWeb.ProfileControllerTest do
     conn = get(conn, Routes.v1_profile_path(conn, :show_current_profile))
 
     assert json_response(conn, 200)["name"] == current_user.name
-  end
-
-  test "deletes profile and render profile details", %{conn: conn} do
-    profile = insert(:profile)
-
-    conn =
-      build_conn()
-      |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
-      |> assign(:current_user, conn.assigns.current_user)
-
-    count_before = Profile |> Repo.all() |> length
-
-    conn = delete(conn, Routes.v1_profile_path(conn, :delete, profile.uuid))
-    count_after = Profile |> Repo.all() |> length()
-    assert count_before - 1 == count_after
-    assert json_response(conn, 200)["name"] == profile.name
   end
 end
