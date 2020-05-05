f3b8c94ec307cc36804037c6a1b250c8a513e44b
Shijith K <shijithjayan@gmail.com>
2020-05-05 15:56:43 +0530

    test: bulk data template creation

--- test/wraft_doc/document/document_test.exs
+++ test/wraft_doc/document/document_test.exs
@@ -55,4 +55,31 @@ defmodule WraftDoc.DocumentTest do
     assert layout.unit == @valid_layout_attrs["unit"]
     assert layout.slug == @valid_layout_attrs["slug"]
   end
+
+  describe "data_template_bulk_insert/4" do
+    test "test bulk data template creation with valid data" do
+      c_type = insert(:content_type)
+      user = insert(:user)
+      mapping = %{"Title" => "title", "TitleTemplate" => "title_template", "Data" => "data"}
+      path = "test/helper/data_template_source.csv"
+      count_before = DataTemplate |> Repo.all() |> length()
+
+      data_templates =
+        Document.data_template_bulk_insert(user, c_type, mapping, path)
+        |> Enum.map(fn {:ok, x} -> x.title end)
+        |> List.to_string()
+
+      assert count_before + 3 == DataTemplate |> Repo.all() |> length()
+      assert data_templates =~ "Title1"
+      assert data_templates =~ "Title2"
+      assert data_templates =~ "Title3"
+    end
+
+    test "test doesn not do bulk data template creation with invalid data" do
+      count_before = DataTemplate |> Repo.all() |> length()
+      response = Document.data_template_bulk_insert(nil, nil, nil, nil)
+      assert count_before == DataTemplate |> Repo.all() |> length()
+      assert response == {:error, :not_found}
+    end
+  end
 end
