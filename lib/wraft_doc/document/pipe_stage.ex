434f64b7bb7ea99158a34834eb48308c3d49250c
Shijith K <shijithjayan@gmail.com>
2020-05-08 20:52:41 +0530

    feat: delete pipe stage

--- lib/wraft_doc/document/pipe_stage.ex
+++ lib/wraft_doc/document/pipe_stage.ex
@@ -3,8 +3,24 @@ defmodule WraftDoc.Document.Pipeline.Stage do
   The pipeline stages model.
   """
   alias __MODULE__
+  alias WraftDoc.{Account.User, Document.Pipeline}
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
+
+  defimpl Spur.Trackable, for: Stage do
+    def actor(_stage), do: ""
+    def object(stage), do: "Stage:#{stage.id}"
+    def target(_chore), do: nil
+
+    def audience(%{pipeline_id: id}) do
+      from(u in User,
+        join: p in Pipeline,
+        where: p.id == ^id,
+        where: u.organisation_id == p.organisation_id
+      )
+    end
+  end
 
   schema "pipe_stage" do
     field(:uuid, Ecto.UUID, autogenerate: true)
