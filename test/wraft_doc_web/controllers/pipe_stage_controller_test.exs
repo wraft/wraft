dceed41878fc5973040290c8ea365aa1be682953
Shijith K <shijithjayan@gmail.com>
2020-05-13 17:05:42 +0530

    test: updated pipestage conttoller update action test

--- test/wraft_doc_web/controllers/pipe_stage_controller_test.exs
+++ test/wraft_doc_web/controllers/pipe_stage_controller_test.exs
@@ -209,6 +209,126 @@ defmodule WraftDocWeb.Api.V1.PipeStageControllerTest do
       assert json_response(conn, 200)["state"]["id"] == state.uuid
     end
 
+    test "does not update pipe stage and returns not found with non existent datas in attrs", %{
+      conn: conn
+    } do
+      user = conn.assigns.current_user
+
+      conn =
+        build_conn()
+        |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
+        |> assign(:current_user, user)
+
+      params = %{
+        content_type_id: Ecto.UUID.generate(),
+        data_template_id: Ecto.UUID.generate(),
+        state_id: Ecto.UUID.generate()
+      }
+
+      count_before = Stage |> Repo.all() |> length()
+
+      conn =
+        put(conn, Routes.v1_pipe_stage_path(conn, :update, Ecto.UUID.generate()), params)
+        |> doc(operation_id: "update_pipe_stage")
+
+      assert count_before == Stage |> Repo.all() |> length()
+      assert json_response(conn, 404) == "Not Found"
+    end
+
+    test "does not update pipe stage and returns not found for datas from different organisation of user",
+         %{conn: conn} do
+      user = conn.assigns.current_user
+
+      conn =
+        build_conn()
+        |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
+        |> assign(:current_user, user)
+
+      pipeline = insert(:pipeline, organisation: user.organisation)
+      stage = insert(:pipe_stage, pipeline: pipeline)
+      c_type = insert(:content_type)
+      d_temp = insert(:data_template)
+      state = insert(:state)
+
+      params = %{
+        content_type_id: c_type.uuid,
+        data_template_id: d_temp.uuid,
+        state_id: state.uuid
+      }
+
+      count_before = Stage |> Repo.all() |> length()
+
+      conn =
+        put(conn, Routes.v1_pipe_stage_path(conn, :update, stage.uuid), params)
+        |> doc(operation_id: "update_pipe_stage")
+
+      assert count_before == Stage |> Repo.all() |> length()
+      assert json_response(conn, 404) == "Not Found"
+    end
+
+    test "does not update pipe stage and returns not found with invalid datas", %{conn: conn} do
+      user = conn.assigns.current_user
+
+      conn =
+        build_conn()
+        |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
+        |> assign(:current_user, user)
+
+      pipeline = insert(:pipeline, organisation: user.organisation)
+      stage = insert(:pipe_stage, pipeline: pipeline)
+      params = %{content_type_id: 3, data_template_id: 2, state_id: 1}
+
+      count_before = Stage |> Repo.all() |> length()
+
+      conn =
+        put(conn, Routes.v1_pipe_stage_path(conn, :update, stage.uuid), params)
+        |> doc(operation_id: "update_pipe_stage")
+
+      assert count_before == Stage |> Repo.all() |> length()
+      assert json_response(conn, 404) == "Not Found"
+    end
+
+    test "does not update pipe stage and returns error when content type and pipeline ID are same as a previously created stage",
+         %{conn: conn} do
+      user = conn.assigns.current_user
+
+      conn =
+        build_conn()
+        |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
+        |> assign(:current_user, user)
+
+      pipeline = insert(:pipeline, organisation: user.organisation)
+      c_type = insert(:content_type, organisation: user.organisation)
+      d_temp = insert(:data_template, content_type: c_type)
+      state = insert(:state, organisation: user.organisation)
+
+      insert(:pipe_stage,
+        pipeline: pipeline,
+        content_type: c_type,
+        data_template: d_temp,
+        state: state
+      )
+
+      stage = insert(:pipe_stage, pipeline: pipeline)
+
+      params = %{
+        content_type_id: c_type.uuid,
+        data_template_id: d_temp.uuid,
+        state_id: state.uuid
+      }
+
+      count_before = Stage |> Repo.all() |> length()
+
+      conn =
+        put(conn, Routes.v1_pipe_stage_path(conn, :update, stage.uuid), params)
+        |> doc(operation_id: "update_pipe_stage")
+
+      assert count_before == Stage |> Repo.all() |> length()
+      assert json_response(conn, 422)["errors"]["content_type_id"] == ["Already added.!"]
+    end
+  end
+
+  describe "delete" do
     test "delete pipe stage by given id", %{conn: conn} do
       user = conn.assigns.current_user
 
@@ -237,119 +357,5 @@ defmodule WraftDocWeb.Api.V1.PipeStageControllerTest do
 
       assert json_response(conn, 404) == "Not Found"
     end
-
-    # test "does not create pipe stage and returns not found with non existent datas in attrs", %{
-    #   conn: conn
-    # } do
-    #   user = conn.assigns.current_user
-
-    #   conn =
-    #     build_conn()
-    #     |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
-    #     |> assign(:current_user, user)
-
-    #   params = %{
-    #     content_type_id: Ecto.UUID.generate(),
-    #     data_template_id: Ecto.UUID.generate(),
-    #     state_id: Ecto.UUID.generate()
-    #   }
-
-    #   count_before = Stage |> Repo.all() |> length()
-
-    #   conn =
-    #     post(conn, Routes.v1_pipe_stage_path(conn, :create, Ecto.UUID.generate()), params)
-    #     |> doc(operation_id: "create_pipe_stage")
-
-    #   assert count_before == Stage |> Repo.all() |> length()
-    #   assert json_response(conn, 404) == "Not Found"
-    # end
-
-    # test "does not create pipe stage and returns not found for datas from different organisation of user",
-    #      %{conn: conn} do
-    #   user = conn.assigns.current_user
-
-    #   conn =
-    #     build_conn()
-    #     |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
-    #     |> assign(:current_user, user)
-
-    #   pipeline = insert(:pipeline, organisation: user.organisation)
-    #   c_type = insert(:content_type)
-    #   d_temp = insert(:data_template)
-    #   state = insert(:state)
-
-    #   params = %{
-    #     content_type_id: c_type.uuid,
-    #     data_template_id: d_temp.uuid,
-    #     state_id: state.uuid
-    #   }
-
-    #   count_before = Stage |> Repo.all() |> length()
-
-    #   conn =
-    #     post(conn, Routes.v1_pipe_stage_path(conn, :create, pipeline.uuid), params)
-    #     |> doc(operation_id: "create_pipe_stage")
-
-    #   assert count_before == Stage |> Repo.all() |> length()
-    #   assert json_response(conn, 404) == "Not Found"
-    # end
-
-    # test "does not create pipe stage and returns not found with invalid datas", %{conn: conn} do
-    #   user = conn.assigns.current_user
-
-    #   conn =
-    #     build_conn()
-    #     |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
-    #     |> assign(:current_user, user)
-
-    #   pipeline = insert(:pipeline, organisation: user.organisation)
-    #   params = %{content_type_id: 3, data_template_id: 2, state_id: 1}
-
-    #   count_before = Stage |> Repo.all() |> length()
-
-    #   conn =
-    #     post(conn, Routes.v1_pipe_stage_path(conn, :create, pipeline.uuid), params)
-    #     |> doc(operation_id: "create_pipe_stage")
-
-    #   assert count_before == Stage |> Repo.all() |> length()
-    #   assert json_response(conn, 404) == "Not Found"
-    # end
-
-    # test "does not create pipe stage and returns error when content type and pipeline ID are same as a previously created stage",
-    #      %{conn: conn} do
-    #   user = conn.assigns.current_user
-
-    #   conn =
-    #     build_conn()
-    #     |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
-    #     |> assign(:current_user, user)
-
-    #   pipeline = insert(:pipeline, organisation: user.organisation)
-    #   c_type = insert(:content_type, organisation: user.organisation)
-    #   d_temp = insert(:data_template, content_type: c_type)
-    #   state = insert(:state, organisation: user.organisation)
-
-    #   insert(:pipe_stage,
-    #     pipeline: pipeline,
-    #     content_type: c_type,
-    #     data_template: d_temp,
-    #     state: state
-    #   )
-
-    #   params = %{
-    #     content_type_id: c_type.uuid,
-    #     data_template_id: d_temp.uuid,
-    #     state_id: state.uuid
-    #   }
-
-    #   count_before = Stage |> Repo.all() |> length()
-
-    #   conn =
-    #     post(conn, Routes.v1_pipe_stage_path(conn, :create, pipeline.uuid), params)
-    #     |> doc(operation_id: "create_pipe_stage")
-
-    #   assert count_before == Stage |> Repo.all() |> length()
-    #   assert json_response(conn, 422)["errors"]["content_type_id"] == ["Already added.!"]
-    # end
   end
 end
