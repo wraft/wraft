7c78e79b6ae41a1e9888ccce0269ec2b76d2e8a2
Shijith K <shijithjayan@gmail.com>
2020-05-13 15:12:26 +0530

    test: updated pipestage test

--- test/wraft_doc/document/pipe_stage_test.exs
+++ test/wraft_doc/document/pipe_stage_test.exs
@@ -4,6 +4,39 @@ defmodule WraftDoc.Document.Pipeline.StageTest do
   import WraftDoc.Factory
   import Ecto
 
+  test "changeset with valid attrs" do
+    %{id: u_id} = insert(:user)
+    %{id: c_id} = insert(:content_type)
+    pipeline = insert(:pipeline)
+    %{id: d_id} = insert(:data_template)
+    %{id: s_id} = insert(:state)
+
+    stage_struct =
+      pipeline
+      |> build_assoc(:stages,
+        content_type_id: c_id,
+        data_template_id: d_id,
+        state_id: s_id,
+        creator_id: u_id
+      )
+
+    changeset = Stage.changeset(stage_struct, %{})
+
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attrs" do
+    pipeline = insert(:pipeline)
+
+    stage_struct =
+      pipeline
+      |> build_assoc(:stages, content_type_id: "")
+
+    changeset = Stage.changeset(stage_struct, %{})
+
+    refute changeset.valid?
+  end
+
   test "pipeline stage unique constraint" do
     %{id: u_id} = insert(:user)
     %{id: c_id} = insert(:content_type)
@@ -24,5 +57,27 @@ defmodule WraftDoc.Document.Pipeline.StageTest do
     {:error, changeset} = Stage.changeset(stage_struct, %{}) |> Repo.insert()
 
     assert "Already added.!" in errors_on(changeset, :content_type_id)
+  end
+
+  test "update changeset with valid attrs" do
+    stage = insert(:pipe_stage)
+    %{id: c_id} = insert(:content_type)
+    %{id: d_id} = insert(:data_template)
+    %{id: s_id} = insert(:state)
+
+    changeset =
+      Stage.update_changeset(stage, %{
+        content_type_id: c_id,
+        data_template_id: d_id,
+        state_id: s_id
+      })
+
+    assert changeset.valid?
+  end
+
+  test "update changeset with invalid attrs" do
+    stage = insert(:pipe_stage)
+    changeset = Stage.update_changeset(stage, %{content_type_id: ""})
+    refute changeset.valid?
   end
 end
