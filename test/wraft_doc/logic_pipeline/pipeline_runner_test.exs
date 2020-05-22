4320ca91f4b760945e27ee3811cce72e18cfec8a
Shijith K <shijithjayan@gmail.com>
2020-05-22 13:50:34 +0530

    test: added test for build function in pipeline runner

--- test/wraft_doc/logic_pipeline/pipeline_runner_test.exs
+++ test/wraft_doc/logic_pipeline/pipeline_runner_test.exs
@@ -2,7 +2,7 @@ defmodule WraftDoc.PipelineRunnerTest do
   use WraftDoc.DataCase, async: true
   import WraftDoc.Factory
   use ExUnit.Case
-  alias WraftDoc.{PipelineRunner, Document.Instance}
+  alias WraftDoc.{PipelineRunner, Document.Instance, Document.Instance.History}
 
   describe "preload_pipeline_and_stages/1" do
     test "returns preloaded trigger struct with trigger struct as input" do
@@ -162,6 +162,40 @@ defmodule WraftDoc.PipelineRunnerTest do
         PipelineRunner.instances_created?(%{instances: [instance1, {:error, %Ecto.Changeset{}}]})
 
       assert response == false
+    end
+  end
+
+  describe "build/1" do
+    test "builds the list of instances in the map and returns a map with with the instances and build responses when input map has a user key" do
+      user = insert(:user)
+      instances = insert_list(3, :instance)
+      count_before = History |> Repo.all() |> length()
+      response = PipelineRunner.build(%{instances: instances, user: user})
+
+      instance_ids = instances |> Enum.map(fn x -> x.id end)
+
+      response_instance_ids =
+        response.builds |> Enum.map(fn %{instance: instance} -> instance.id end)
+
+      assert count_before + 3 == History |> Repo.all() |> length()
+      assert response.instances == instances
+      assert response.user == user
+      assert response_instance_ids == instance_ids
+    end
+
+    test "builds the list of instances in the map and returns a map with with the instances and build responses when input map does not have a user key" do
+      instances = insert_list(3, :instance)
+      count_before = History |> Repo.all() |> length()
+      response = PipelineRunner.build(%{instances: instances})
+
+      instance_ids = instances |> Enum.map(fn x -> x.id end)
+
+      response_instance_ids =
+        response.builds |> Enum.map(fn %{instance: instance} -> instance.id end)
+
+      assert count_before + 3 == History |> Repo.all() |> length()
+      assert response.instances == instances
+      assert response_instance_ids == instance_ids
     end
   end
 end
