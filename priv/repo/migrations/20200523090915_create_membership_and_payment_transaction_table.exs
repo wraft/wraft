11686c420caca916afb22500b7a60568237fc7f4
Shijith K <shijith.k@aurut.com>
2020-05-23 15:05:47 +0530

    feat: billing table migrations

---
+++ priv/repo/migrations/20200523090915_create_membership_and_payment_transaction_table.exs
@@ -0,0 +1,41 @@
+defmodule WraftDoc.Repo.Migrations.CreateMembershipAndPaymentTransactionTable do
+  use Ecto.Migration
+
+  def up do
+    create table(:membership) do
+      add(:uuid, :uuid, null: false)
+      add(:organisation_id, references(:organisation, on_delete: :nilify_all))
+      add(:plan_id, references(:plan, on_delete: :nilify_all))
+      add(:start_date, :naive_datetime)
+      add(:end_date, :naive_datetime)
+      add(:plan_duration, :integer)
+
+      timestamps()
+    end
+
+    create table(:payment) do
+      add(:uuid, :uuid, null: false)
+      add(:organisation_id, references(:organisation, on_delete: :nilify_all))
+      add(:creator_id, references(:user, on_delete: :nilify_all))
+      add(:membership_id, references(:membership, on_delete: :nilify_all))
+      add(:razerpay_id, :string, null: false)
+      add(:start_date, :naive_datetime)
+      add(:end_date, :naive_datetime)
+      add(:invoice, :string)
+      add(:invoice_number, :string)
+      add(:amount, :float, default: 0.0)
+      add(:action, :string)
+      add(:from_plan_id, references(:plan, on_delete: :nilify_all))
+      add(:to_plan_id, references(:plan, on_delete: :nilify_all))
+
+      timestamps()
+    end
+
+    create(unique_index(:membership, [:organisation_id]))
+  end
+
+  def down do
+    drop_if_exists(table(:payment))
+    drop_if_exists(table(:membership))
+  end
+end
