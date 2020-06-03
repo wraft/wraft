c29d79ae56b22a81a51da76f23faaeefcd8a0cb1
Shijith K <shijithjayan@gmail.com>
2020-06-03 23:31:52 +0530

    test: improved action logging tests

--- test/wraft_doc_web/plug/action_logging_test.exs
+++ test/wraft_doc_web/plug/action_logging_test.exs
@@ -4,52 +4,28 @@ defmodule WraftDocWeb.Plug.AddActionLogTest do
   alias WraftDoc.{Repo, ActionLog}
   alias WraftDocWeb.Plug.AddActionLog
 
-  setup %{conn: conn} do
+  test "adds new log when an action is made by an authorized user" do
     user = insert(:user)
-
-    conn =
-      conn
-      |> put_req_header("accept", "application/json")
-      |> post(
-        Routes.v1_user_path(conn, :signin, %{
-          email: user.email,
-          password: user.password
-        })
-      )
-
-    conn = assign(conn, :current_user, user)
-
-    {:ok, %{conn: conn}}
-  end
-
-  test "adds new log when an action is made by an authorized user", %{conn: conn} do
-    user = conn.assigns.current_user
     insert(:membership, organisation: user.organisation)
 
     conn =
       build_conn()
-      |> put_req_header("authorization", "Bearer #{conn.assigns.token}")
       |> assign(:current_user, user)
+      |> put_req_header("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4)")
+      |> put_private(:phoenix_action, :test)
+      |> Map.put(:request_path, "/test")
+      |> Map.put(:method, "POST")
+      |> Map.put(:params, %{test: "test"})
 
-    file = Plug.Upload.random_file!("test")
-
-    valid_attrs = %{
-      name: "Theme",
-      font: "Malery",
-      typescale: %{h1: "10", p: "6", h2: "8"},
-      organisation_id: user.organisation_id,
-      file: %Plug.Upload{filename: file, path: file}
-    }
-
-    conn = post(conn, Routes.v1_theme_path(conn, :create), valid_attrs)
     count_before = ActionLog |> Repo.all() |> length
     AddActionLog.call(conn, %{})
     all_actions = ActionLog |> Repo.all()
     last_action = all_actions |> List.last()
 
     assert count_before + 1 == all_actions |> length
-    assert last_action.action == "create"
-    assert last_action.request_path == Routes.v1_theme_path(conn, :create)
+    assert last_action.action == "test"
+
+    assert last_action.request_path == "/test"
     assert last_action.user_id == user.id
 
     assert last_action.actor == %{
@@ -60,12 +36,11 @@ defmodule WraftDocWeb.Plug.AddActionLogTest do
   end
 
   test "adds new log when an action is made by an unauthorized user" do
-    conn = build_conn()
-
-    user = insert(:user, password: "encrypt")
-
     conn =
-      post(conn, Routes.v1_user_path(conn, :signin, %{email: user.email, password: "encrypt"}))
+      build_conn()
+      |> put_req_header("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4)")
+      |> put_private(:phoenix_action, :test)
+      |> Map.put(:params, %{})
 
     count_before = ActionLog |> Repo.all() |> length
     AddActionLog.call(conn, %{})
@@ -73,8 +48,8 @@ defmodule WraftDocWeb.Plug.AddActionLogTest do
     last_action = all_actions |> List.last()
 
     assert count_before + 1 == all_actions |> length
-    assert last_action.action == "signin"
-    assert last_action.request_path == Routes.v1_user_path(conn, :signin)
+    assert last_action.action == "test"
+    assert last_action.request_path == "/"
     assert last_action.user_id == nil
     assert last_action.actor == %{}
   end
