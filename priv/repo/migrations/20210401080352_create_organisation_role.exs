74efb1a9ea1faed32feecba2a0c4073d9741b301
Siddhant <siddant.singh@aurut.com>
2021-04-01 17:40:15 +0530

    fix: uuid added in org_role migration

--- priv/repo/migrations/20210401080352_create_organisation_role.exs
+++ priv/repo/migrations/20210401080352_create_organisation_role.exs
@@ -3,6 +3,7 @@ defmodule WraftDoc.Repo.Migrations.CreateOrganisationRole do
 
   def change do
     create table(:organisation_role) do
+      add :uuid, :uuid, null: false, autogenerate: true
       add :organisation_id, references(:organisation, on_delete: :delete_all)
       add :role_id, references(:role, on_delete: :delete_all)
 
