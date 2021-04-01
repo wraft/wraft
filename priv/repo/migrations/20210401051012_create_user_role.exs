7cabcebe6bb10ccac3871fe033826e7e4acb5b06
Mohammed sadique palakkal <sadique.pal@gmail.com>
2021-04-01 17:20:49 +0530

    feat: query to copy user role data

--- priv/repo/migrations/20210401051012_create_user_role.exs
+++ priv/repo/migrations/20210401051012_create_user_role.exs
@@ -1,11 +1,21 @@
 defmodule WraftDoc.Repo.Migrations.CreateUserRole do
   use Ecto.Migration
 
-  def change do
+  def up do
    create table(:user_role) do
+    add :uuid, :uuid, null: false, autogenerate: true
     add :user_id, references(:user,on_delete: :delete_all)
     add :role_id, references(:role, on_delete: :delete_all)
     timestamps()
+
    end
+
+   execute """
+   INSERT INTO public.user_role (role_id,user_id,uuid, inserted_at, updated_at) SELECT role_id, id, '#{Ecto.UUID.autogenerate()}', '#{NaiveDateTime.local_now()}', '#{NaiveDateTime.local_now()}'  FROM public.user;
+   """
+
+  end
+  def down do
+    drop table(:user_role)
   end
 end
