2e718cfe4ef030fac34832ac689f8e08ca2f1cd3
Mohammed sadique palakkal <sadique.pal@gmail.com>
2021-05-04 15:59:15 +0530

    chore: refactor seeds to prevent errors on repeat

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -31,23 +31,38 @@ alias WraftDoc.{
 }
 
 # Populate database with roles
-Repo.insert!(%Role{name: "admin"})
-Repo.insert!(%Role{name: "super_admin"})
-Repo.insert!(%Role{name: "user"})
+with nil <- Repo.get_by(Role, name: "admin") do
+  Repo.insert!(%Role{name: "admin"})
+end
+
+with nil <- Repo.get_by(Role, name: "super_admin") do
+  Repo.insert!(%Role{name: "super_admin"})
+end
+
+with nil <- Repo.get_by(Role, name: "user") do
+  Repo.insert!(%Role{name: "user"})
+end
 
 # Populate DB with admin user and profile
-%{id: id} = Repo.get_by(Role, name: "super_admin")
 
 # Populate DB with one organisation
+%{id: role_id} = Repo.get_by(Role, name: "super_admin")
+
 organisation =
-  Repo.insert!(%Organisation{
-    name: "Functionary Labs Pvt Ltd.",
-    legal_name: "Functionary Labs Pvt Ltd",
-    address: "#24, Caravel Building",
-    name_of_ceo: "Muneef Hameed",
-    name_of_cto: "Salsabeel K",
-    email: "hello@aurut.com"
-  })
+  case Repo.get_by(Organisation, email: "hello@aurut.com") do
+    %Organisation{} = organisation ->
+      organisation
+
+    _ ->
+      Repo.insert!(%Organisation{
+        name: "Functionary Labs Pvt Ltd.",
+        legal_name: "Functionary Labs Pvt Ltd",
+        address: "#24, Caravel Building",
+        name_of_ceo: "Muneef Hameed",
+        name_of_cto: "Salsabeel K",
+        email: "hello@aurut.com"
+      })
+  end
 
 user_params = %{
   name: "Admin",
@@ -57,125 +72,193 @@ user_params = %{
   organisation_id: organisation.id
 }
 
-%{id: u_id} = user = %User{} |> User.changeset(user_params) |> Repo.insert!()
-Repo.insert!(%Profile{name: "Admin", user_id: user.id})
-Repo.insert!(%UserRole{user_id: u_id, role_id: id})
+user =
+  case Repo.get_by(User, name: "Admin") do
+    %User{} = user ->
+      user
+
+    _ ->
+      %User{} |> User.changeset(user_params) |> Repo.insert!()
+  end
+
+with nil <- Repo.get_by(UserRole, user_id: user.id) do
+  Repo.insert!(%UserRole{user_id: user.id, role_id: role_id})
+end
+
+with nil <- Repo.get_by(Profile, name: "Admin") do
+  Repo.insert!(%Profile{name: "Admin", user_id: user.id})
+end
 
 # Populate engine
-engine = Repo.insert!(%Engine{name: "PDF"})
-Repo.insert!(%Engine{name: "LaTex"})
-Repo.insert!(%Engine{name: "Pandoc"})
+engine =
+  case Repo.get_by(Engine, name: "PDF") do
+    %Engine{} = engine -> engine
+    _ -> Repo.insert!(%Engine{name: "PDF"})
+  end
+
+with nil <- Repo.get_by(Engine, name: "LaTex") do
+  Repo.insert!(%Engine{name: "LaTex"})
+end
+
+with nil <- Repo.get_by(Engine, name: "Pandoc") do
+  Repo.insert!(%Engine{name: "Pandoc"})
+end
 
 # Populate layout
+
 layout =
-  Repo.insert!(%Layout{
-    name: "Official Letter",
-    description: "An official letter",
-    width: 30.0,
-    height: 40.0,
-    unit: "cm",
-    slug: "pletter",
-    engine_id: engine.id,
-    creator_id: user.id,
-    organisation_id: organisation.id
-  })
+  case Repo.get_by(Layout, slug: "pletter") do
+    %Layout{} = layout ->
+      layout
+
+    _ ->
+      Repo.insert!(%Layout{
+        name: "Official Letter",
+        description: "An official letter",
+        width: 30.0,
+        height: 40.0,
+        unit: "cm",
+        slug: "pletter",
+        engine_id: engine.id,
+        creator_id: user.id,
+        organisation_id: organisation.id
+      })
+  end
 
 # Populate fields
-field = Repo.insert!(%FieldType{name: "String", creator_id: user.id})
+field =
+  case Repo.get_by(FieldType, name: "String") do
+    %FieldType{} = field -> field
+    _ -> Repo.insert!(%FieldType{name: "String", creator_id: user.id})
+  end
 
 # Populate flow
 flow =
-  Repo.insert!(%Flow{
-    name: "Flow 1",
-    organisation_id: organisation.id,
-    creator_id: user.id
-  })
+  case Repo.get_by(Flow, name: "Flow 1") do
+    %Flow{} = flow ->
+      flow
+
+    _ ->
+      Repo.insert!(%Flow{
+        name: "Flow 1",
+        organisation_id: organisation.id,
+        creator_id: user.id
+      })
+  end
 
 # Populate Content Type
 content_type =
-  Repo.insert!(%ContentType{
-    name: "Offer Letter",
-    description: "An offer letter",
-    prefix: "OFFLET",
-    layout_id: layout.id,
-    creator_id: user.id,
-    organisation_id: organisation.id,
-    flow_id: flow.id,
-    color: "#fff"
-  })
+  case Repo.get_by(ContentType, prefix: "OFFLET") do
+    %ContentType{} = content_type ->
+      content_type
+
+    _ ->
+      Repo.insert!(%ContentType{
+        name: "Offer Letter",
+        description: "An offer letter",
+        prefix: "OFFLET",
+        layout_id: layout.id,
+        creator_id: user.id,
+        organisation_id: organisation.id,
+        flow_id: flow.id,
+        color: "#fff"
+      })
+  end
 
 # Populate content type fields
-Repo.insert!(%ContentTypeField{
-  name: "employee",
-  content_type_id: content_type.id,
-  field_type_id: field.id
-})
+with nil <- Repo.get_by(ContentTypeField, name: "employee") do
+  Repo.insert!(%ContentTypeField{
+    name: "employee",
+    content_type_id: content_type.id,
+    field_type_id: field.id
+  })
+end
 
 # Populate State
 state =
-  Repo.insert!(%State{
-    state: "Published",
-    order: 1,
-    creator_id: user.id,
-    organisation_id: organisation.id,
-    flow_id: flow.id
-  })
+  case Repo.get_by(State, state: "Published") do
+    %State{} = state ->
+      state
+
+    _ ->
+      Repo.insert!(%State{
+        state: "Published",
+        order: 1,
+        creator_id: user.id,
+        organisation_id: organisation.id,
+        flow_id: flow.id
+      })
+  end
 
 # Populate Instance
-Repo.insert!(%Instance{
-  instance_id: "OFFLET0001",
-  raw: "Hi John Doe, We offer you the position of Elixir developer",
-  serialized: %{
-    title: "Offer Letter for Elixir",
-    body: "Hi John Doe, We offer you the position of Elixir developer"
-  },
-  creator_id: user.id,
-  content_type_id: content_type.id,
-  state_id: state.id
-})
+with nil <- Repo.get_by(Instance, instance_id: "OFFLET0001") do
+  Repo.insert!(%Instance{
+    instance_id: "OFFLET0001",
+    raw: "Hi John Doe, We offer you the position of Elixir developer",
+    serialized: %{
+      title: "Offer Letter for Elixir",
+      body: "Hi John Doe, We offer you the position of Elixir developer"
+    },
+    creator_id: user.id,
+    content_type_id: content_type.id,
+    state_id: state.id
+  })
+end
 
 # Populate Counter
-Repo.insert!(%Counter{
-  subject: "ContentType:#{content_type.id}",
-  count: 1
-})
+with nil <- Repo.get_by(Counter, subject: "ContentType:#{content_type.id}") do
+  Repo.insert!(%Counter{
+    subject: "ContentType:#{content_type.id}",
+    count: 1
+  })
+end
 
 # Populate theme
-Repo.insert!(%Theme{
-  name: "Offer letter theme",
-  font: "Malery",
-  typescale: %{h1: 10, h2: 8, p: 6},
-  creator_id: user.id,
-  organisation_id: organisation.id
-})
+with nil <- Repo.get_by(Theme, font: "Malery") do
+  Repo.insert!(%Theme{
+    name: "Offer letter theme",
+    font: "Malery",
+    typescale: %{h1: 10, h2: 8, p: 6},
+    creator_id: user.id,
+    organisation_id: organisation.id
+  })
+end
 
 # Populate data template
-Repo.insert!(%DataTemplate{
-  title: "Offer letter tempalate",
-  title_template: "Offer Letter for [employee]",
-  data: "Hi [employee], we welcome you to our [company]",
-  content_type_id: content_type.id,
-  creator_id: user.id
-})
+with nil <- Repo.get_by(DataTemplate, content_type_id: content_type.id) do
+  Repo.insert!(%DataTemplate{
+    title: "Offer letter tempalate",
+    title_template: "Offer Letter for [employee]",
+    data: "Hi [employee], we welcome you to our [company]",
+    content_type_id: content_type.id,
+    creator_id: user.id
+  })
+end
 
 # Populate plans
-Repo.insert!(%Plan{
-  name: "Free Trial",
-  description: "Free trial where users can try out all the features",
-  yearly_amount: 0,
-  monthly_amount: 0
-})
+with nil <- Repo.get_by(Plan, name: "Free Trial") do
+  Repo.insert!(%Plan{
+    name: "Free Trial",
+    description: "Free trial where users can try out all the features",
+    yearly_amount: 0,
+    monthly_amount: 0
+  })
+end
 
-Repo.insert!(%Plan{
-  name: "Premium",
-  description: "Premium plan with premium features only",
-  yearly_amount: 0,
-  monthly_amount: 0
-})
+with nil <- Repo.get_by(Plan, name: "Premium") do
+  Repo.insert!(%Plan{
+    name: "Premium",
+    description: "Premium plan with premium features only",
+    yearly_amount: 0,
+    monthly_amount: 0
+  })
+end
 
-Repo.insert!(%Plan{
-  name: "Pro",
-  description: "Pro plan suitable for enterprises",
-  yearly_amount: 0,
-  monthly_amount: 0
-})
+with nil <- Repo.get_by(Plan, name: "Pro") do
+  Repo.insert!(%Plan{
+    name: "Pro",
+    description: "Pro plan suitable for enterprises",
+    yearly_amount: 0,
+    monthly_amount: 0
+  })
+end
