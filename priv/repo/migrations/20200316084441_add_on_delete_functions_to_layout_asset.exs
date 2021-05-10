c2b9ea72be62a5502dff2fd71a74fd828e2c7fea
Sadique <msadiquep@gmail.com>
2021-05-10 17:54:20 +0530

    refactor: layout asset

--- priv/repo/migrations/20200316084441_add_on_delete_functions_to_layout_asset.exs
+++ priv/repo/migrations/20200316084441_add_on_delete_functions_to_layout_asset.exs
@@ -2,15 +2,10 @@ defmodule WraftDoc.Repo.Migrations.AddOnDeleteFunctionsToLayoutAsset do
   use Ecto.Migration
 
   def up do
-    drop(constraint(:layout_asset, "layout_asset_layout_id_fkey"))
-    drop(constraint(:layout_asset, "layout_asset_asset_id_fkey"))
-
     create(unique_index(:layout_asset, [:layout_id, :asset_id], name: :layout_asset_unique_index))
   end
 
   def down do
-    drop(constraint(:layout_asset, "layout_asset_layout_id_fkey"))
-    drop(constraint(:layout_asset, "layout_asset_asset_id_fkey"))
     drop(unique_index(:layout_asset, [:layout_id, :asset_id], name: :layout_asset_unique_index))
   end
 end
