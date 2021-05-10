145b6fd37936b77fa43ec3840622d0aefb1a07d9
Siddhant Singh <ssiddhant3030@gmail.com>
2021-05-10 17:54:29 +0530

    refactor: version migration table

--- priv/repo/migrations/20200415075716_create_versions_table.exs
+++ priv/repo/migrations/20200415075716_create_versions_table.exs
@@ -6,9 +6,10 @@ defmodule WraftDoc.Repo.Migrations.CreateVersionsTable do
       add(:id, :uuid, primary_key: true)
       add(:version_number, :integer, default: 1)
       add(:raw, :string)
+      add(:naration, :string)
       add(:serialized, :jsonb)
       add(:content_id, references(:content, type: :uuid, column: :id, on_delete: :nilify_all))
-      add(:creator_id, references(:user, type: :uuid, column: :id, on_delete: :nilify_all))
+      add(:author_id, references(:user, type: :uuid, column: :id, on_delete: :nilify_all))
       timestamps()
     end
 
