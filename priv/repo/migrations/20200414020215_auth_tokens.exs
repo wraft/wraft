318e4a509a7a68470005923c36768d1352dcba7b
Sadique <msadiquep@gmail.com>
2021-05-10 15:04:50 +0530

    refactor: auth token

--- priv/repo/migrations/20200414020215_auth_tokens.exs
+++ priv/repo/migrations/20200414020215_auth_tokens.exs
@@ -2,12 +2,13 @@ defmodule WraftDoc.Repo.Migrations.AuthTokens do
   use Ecto.Migration
 
   def change do
-    create table(:auth_token) do
-      add(:uuid, :uuid, null: false)
+    create table(:auth_token, primary_key: false) do
+      add(:id, :uuid, primary_key: true)
       add(:value, :string, null: false)
       add(:token_type, :string)
       add(:expiry_datetime, :naive_datetime)
-      add(:user_id, references(:user))
+      add(:user_id, references(:user, type: :uuid, column: :id, on_delete: :nilify_all))
+      timestamps()
     end
   end
 end
