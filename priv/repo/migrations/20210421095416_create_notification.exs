632e8e8f28bfb00c649b396046ecce8bae4cc720
Siddhant Singh <ssiddhant3030@gmail.com>
2021-05-10 17:57:46 +0530

    refactor: notification migration and schema changes

--- priv/repo/migrations/20210421095416_create_notification.exs
+++ priv/repo/migrations/20210421095416_create_notification.exs
@@ -2,15 +2,15 @@ defmodule WraftDoc.Repo.Migrations.CreateNotification do
   use Ecto.Migration
 
   def change do
-    create table(:notification) do
-      add(:uuid, :uuid, null: false, autogenerate: true)
+    create table(:notification, primary_key: false) do
+      add(:uuid, :uuid, primary_key: true)
       add(:read_at, :naive_datetime)
       add(:read, :boolean)
       add(:action, :string)
       add(:notifiable_id, :uuid)
       add(:notifiable_type, :string)
-      add(:recipient_id, references(:user, on_delete: :nilify_all))
-      add(:actor_id, references(:user, on_delete: :nilify_all))
+      add(:recipient_id, references(:user, type: :uuid, column: :id, on_delete: :nilify_all))
+      add(:actor_id, references(:user, type: :uuid, column: :id, on_delete: :nilify_all))
 
       timestamps()
     end
