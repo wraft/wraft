92d0f1d4c80dcaaaf23b59f2a6e85e5f98f41890
Siddhant Singh <ssiddhant3030@gmail.com>
2021-05-10 15:57:33 +0530

    refactor: organisation field

--- lib/wraft_doc/schema.ex
+++ lib/wraft_doc/schema.ex
@@ -6,6 +6,7 @@ defmodule WraftDoc.Schema do
     quote do
       use Ecto.Schema
       import Ecto.Changeset
+      import Ecto.Query
       @primary_key {:id, :binary_id, autogenerate: true}
       @foreign_key_type :binary_id
       @derive {Phoenix.Param, key: :id}
--- priv/repo/migrations/20210417114232_create_organisation_field.exs
+++ priv/repo/migrations/20210417114232_create_organisation_field.exs
@@ -2,14 +2,23 @@ defmodule WraftDoc.Repo.Migrations.CreateOrganisationField do
   use Ecto.Migration
 
   def change do
-    create table(:organisation_field) do
-      add(:uuid, :uuid, null: false, autogenerate: true)
+    create table(:organisation_field, primary_key: false) do
+      add(:id, :uuid, primary_key: true)
       add(:name, :string)
       add(:meta, :jsonb)
       add(:description, :string)
-      add(:field_type_id, references(:field_type, on_delete: :nilify_all))
-      add(:organisation_id, references(:organisation, on_delete: :nilify_all))
-      add(:creator_id, references(:user, on_delete: :nilify_all))
+
+      add(
+        :field_type_id,
+        references(:field_type, type: :uuid, coloumn: :id, on_delete: :nilify_all)
+      )
+
+      add(
+        :organisation_id,
+        references(:organisation, type: :uuid, coloumn: :id, on_delete: :nilify_all)
+      )
+
+      add(:creator_id, references(:user, type: :uuid, coloumn: :id, on_delete: :nilify_all))
       timestamps()
     end
   end
