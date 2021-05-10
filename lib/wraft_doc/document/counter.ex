62a37697ba4b328ec9c870ed497c562fe13778da
Sadique <msadiquep@gmail.com>
2021-05-10 17:16:05 +0530

    refactor: counter and data template

--- lib/wraft_doc/document/counter.ex
+++ lib/wraft_doc/document/counter.ex
@@ -2,12 +2,12 @@ defmodule WraftDoc.Document.Counter do
   @moduledoc """
   The Counter model.
   """
-  use Ecto.Schema
-  import Ecto.Changeset
+  use WraftDoc.Schema
 
   schema "counter" do
     field(:subject, :string)
     field(:count, :integer)
+    timestamps()
   end
 
   def changeset(counter, attrs \\ %{}) do
