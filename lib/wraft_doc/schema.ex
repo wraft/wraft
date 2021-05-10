b0c79d2dd01a3f33a1ec2614e16748328944dbf9
Mohammed sadique palakkal <sadique.pal@gmail.com>
2021-05-10 11:00:40 +0530

    feat: schema macro

---
+++ lib/wraft_doc/schema.ex
@@ -0,0 +1,14 @@
+defmodule WraftDoc.Schema do
+  @moduledoc """
+   Schema Macro
+  """
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+      import Ecto.Changeset
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+      @derive {Phoenix.Param, key: :id}
+    end
+  end
+end
