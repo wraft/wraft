6a61f5c853600255318f1c3936f3ae39c9a6688d
Siddhant Singh <ssiddhant3030@gmail.com>
2021-05-12 15:21:14 +0530

    refactor: content type and approval system

--- .iex.exs
+++ .iex.exs
@@ -7,3 +7,20 @@ alias WraftDoc.Document.ContentType
 alias WraftDoc.Document.ContentTypeRole
 alias WraftDoc.Document
 alias WraftDoc.Account.User
+
+alias WraftDoc.{
+  Account,
+  Account.User,
+  Document,
+  Document.Instance,
+  Enterprise.ApprovalSystem,
+  Enterprise.Flow,
+  Enterprise.Flow.State,
+  Enterprise.Membership,
+  Enterprise.Membership.Payment,
+  Enterprise.Organisation,
+  Enterprise.Plan,
+  Enterprise.Vendor,
+  Notifications,
+  Repo
+}
