4c3202aee7958fcc6c9ff7ddf398cc2e0eb68005
Mohammed sadique palakkal <sadique.pal@gmail.com>
2021-06-21 17:47:50 +0530

    fix: fixed failing tests

--- priv/repo/migrations/20210621103743_add_creator.exs
+++ priv/repo/migrations/20210621103743_add_creator.exs
@@ -4,7 +4,11 @@ defmodule WraftDoc.Repo.Migrations.AddCreator do
   def change do
     alter table(:collection_form) do
       add(:creator_id, references(:user, type: :uuid, column: :id, on_delete: :nilify_all))
-      add(:organisation_id, references(:user, type: :uuid, column: :id, on_delete: :nilify_all))
+
+      add(
+        :organisation_id,
+        references(:organisation, type: :uuid, column: :id, on_delete: :nilify_all)
+      )
     end
 
     alter table(:collection_form_field) do
