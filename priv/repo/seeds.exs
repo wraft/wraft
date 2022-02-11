129f25a667cc269f9f352de872c2be17df86e16d
Ambareesha AV <ambareesha7@gmail.com>
2022-02-11 13:34:51 +0530

    new seeds added

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,6 +9,7 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+# new seeds added
 
 alias WraftDoc.{
   Authorization.Resource,
