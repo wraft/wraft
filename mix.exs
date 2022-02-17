eb550d24ff64c985024974563b303c9100df3501
ambareesha.av <ambareesha.av@aurut.com>
2022-02-17 12:25:37 +0530

    added moore seed data, changed seed_gate.ex location

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule WraftDoc.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:dev), do: ["lib", "priv/repo"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
