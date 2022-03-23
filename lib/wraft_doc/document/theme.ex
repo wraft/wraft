98c436a0613eebb71949abad551eb29b2e53abdf
ambareesha.av <ambareesha.av@aurut.com>
2022-03-23 16:30:02 +0530

    did the changes suggested on merge request

--- lib/wraft_doc/document/theme.ex
+++ lib/wraft_doc/document/theme.ex
@@ -1,23 +1,23 @@
 defmodule WraftDoc.Document.Theme do
-  @hex_code_warning_msg "hex-code must be in the format of `#RRGGBB`"
   @moduledoc """
-    The theme model.
-    ### Fields
-    * `name` - The name of the theme, `:string`
-    * `font` - The font name. `:string`
-    * `typescale` - The type scale to use, example: `{ "p": 6, "h2": 8, "h1": 10}`.
-    * `body_color` - The Body color of the theme, #{@hex_code_warning_msg}.
-    * `primary_color` - The Primary color of the theme, #{@hex_code_warning_msg}.
-    * `secondary_color` - The Secondary color of the theme, #{@hex_code_warning_msg}.
-    * `defualt_theme` - Defualt Theme to use, `true` or `false`
-    * `preview_file` - The Preview file to use. currently supporting formats are `.png` `.jpeg` `.pdf` `.jpg` `.gif`.
-    * `file` - The font file to use. currently supporting formats are `.ttf` `.otf` `.pdf`.
+  The theme model.
+  ### Fields
+  * `name` - The name of the theme, `:string`
+  * `font` - The font name. `:string`
+  * `typescale` - The type scale to use, example: `{ "p": 6, "h2": 8, "h1": 10}`.
+  * `body_color` - The Body color of the theme, hex-code must be in the format of `#RRGGBB`.
+  * `primary_color` - The Primary color of the theme, hex-code must be in the format of `#RRGGBB`.
+  * `secondary_color` - The Secondary color of the theme, hex-code must be in the format of `#RRGGBB`.
+  * `defualt_theme` - Defualt Theme to use, `true` or `false`
+  * `preview_file` - The Preview file to use. currently supporting formats are `.png` `.jpeg` `.pdf` `.jpg` `.gif`.
+  * `file` - The font file to use. currently supporting formats are `.ttf` `.otf` `.pdf`.
   """
   use WraftDoc.Schema
   use Waffle.Ecto.Schema
   alias __MODULE__
   alias WraftDoc.Account.User
 
+  @hex_code_warning_msg "hex-code must be in the format of `#RRGGBB`"
   @derive {Jason.Encoder, only: [:name]}
 
   defimpl Spur.Trackable, for: Theme do
@@ -29,6 +29,17 @@ defmodule WraftDoc.Document.Theme do
       from(u in User, where: u.organisation_id == ^id)
     end
   end
+
+  @fields [
+    :name,
+    :font,
+    :typescale,
+    :organisation_id,
+    :body_color,
+    :primary_color,
+    :secondary_color,
+    :default_theme
+  ]
 
   schema "theme" do
     field(:name, :string, null: false)
@@ -48,22 +59,11 @@ defmodule WraftDoc.Document.Theme do
     timestamps()
   end
 
-  @fields_to_cast [
-    :name,
-    :font,
-    :typescale,
-    :organisation_id,
-    :body_color,
-    :primary_color,
-    :secondary_color,
-    :default_theme
-  ]
-
   @hex_format ~r/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
 
   def changeset(%Theme{} = theme, attrs \\ %{}) do
     theme
-    |> cast(attrs, @fields_to_cast)
+    |> cast(attrs, @fields)
     |> validate_required([:name, :font, :typescale, :organisation_id])
     |> validate_format(:body_color, @hex_format, message: @hex_code_warning_msg)
     |> validate_format(:primary_color, @hex_format, message: @hex_code_warning_msg)
@@ -78,7 +78,7 @@ defmodule WraftDoc.Document.Theme do
 
   def update_changeset(%Theme{} = theme, attrs \\ %{}) do
     theme
-    |> cast(attrs, @fields_to_cast)
+    |> cast(attrs, @fields)
     |> cast_attachments(attrs, [:file, :preview_file])
     |> validate_required([:name, :font, :typescale, :file])
     |> validate_format(:body_color, @hex_format, message: @hex_code_warning_msg)
