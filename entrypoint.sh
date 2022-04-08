b5a719cc4d65a54423e233c491ea7c5fef6572d5
Salsabeel <ambareesha.av@aurut.com>
2022-04-08 20:03:48 +0530

    added docker files

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Docker entry point script.
+# Wait until postgres is ready
+while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+echo "$(date) - PostgreSQL is ready"
+mix ecto.create
+# Create migrate and seed database if it does't exist.
+if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
+  echo "Database $PGDATABASE does not exist. Creating..."
+  createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
+  mix ecto.migrate
+  mix run priv/repo/seeds.exs
+  echo "Database $PGDATABASE created."
+fi
+exec mix phx.server
