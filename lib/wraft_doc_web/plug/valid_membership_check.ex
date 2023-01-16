ebd13e1a0094dfa0ec48907aeacb1e0f443920c9
Vasil <vasilabdulrazak9@gmail.com>
2023-01-16 16:10:45 +0000

    Resolve WD-355 "Feature/ refactor valid membership check plug to use current organisation for the check"

--- lib/wraft_doc_web/plug/valid_membership_check.ex
+++ lib/wraft_doc_web/plug/valid_membership_check.ex
@@ -5,7 +5,9 @@ defmodule WraftDocWeb.Plug.ValidMembershipCheck do
 
   import Plug.Conn
 
-  alias WraftDoc.{Enterprise, Repo}
+  alias WraftDoc.Enterprise
+  alias WraftDoc.Enterprise.Organisation
+  alias WraftDoc.Repo
 
   def init(_params) do
   end
@@ -13,48 +15,36 @@ defmodule WraftDocWeb.Plug.ValidMembershipCheck do
   def call(conn, _params) do
     user = conn.assigns[:current_user]
 
-    case Enum.member?(user.role_names, "super_admin") do
-      true ->
-        conn
-
-      _ ->
-        has_valid_membership?(conn, user)
+    case is_personal_org?(user) do
+      false -> has_valid_membership?(conn, user)
+      true -> conn
     end
   end
 
   # Checks if the user's organisation has a valid membership.
   defp has_valid_membership?(conn, user) do
-    user = Repo.preload(user, [:organisation])
-
-    case Enterprise.get_organisation_membership(user.organisation.id) do
-      %{is_expired: is_expired} ->
-        case is_expired do
-          false ->
-            conn
-
-          true ->
-            body =
-              Jason.encode!(%{
-                errors:
-                  "You do not have a valid membership. Upgrade your membership to continue.!"
-              })
-
-            conn
-            |> put_resp_content_type("application/json")
-            |> send_resp(400, body)
-            |> halt()
-        end
-
-      _ ->
-        body =
-          Jason.encode!(%{
-            errors: "You do not have a valid membership. Upgrade your membership to continue.!"
-          })
-
-        conn
-        |> put_resp_content_type("application/json")
-        |> send_resp(400, body)
-        |> halt()
+    case Enterprise.get_organisation_membership(user.current_org_id) do
+      %{is_expired: false} -> conn
+      _ -> error_response(conn)
     end
+  end
+
+  defp is_personal_org?(user) do
+    case Repo.get_by(Organisation, id: user.current_org_id, name: "Personal") do
+      %Organisation{} -> true
+      nil -> false
+    end
+  end
+
+  defp error_response(conn) do
+    body =
+      Jason.encode!(%{
+        errors: "You do not have a valid membership. Upgrade your membership to continue.!"
+      })
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(400, body)
+    |> halt()
   end
 end
