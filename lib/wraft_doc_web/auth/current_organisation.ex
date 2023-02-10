d36b1913a1f13ed08556d51a66fa93c9a144fd5b
Shijith K <shijithjayan@gmail.com>
2023-02-10 10:40:16 +0000

    [WD-406] Added permissions to current user

--- lib/wraft_doc_web/auth/current_organisation.ex
+++ lib/wraft_doc_web/auth/current_organisation.ex
@@ -1,11 +1,14 @@
 defmodule WraftDocWeb.CurrentOrganisation do
   @moduledoc """
-    This plug stores the current organisation id in the user struct
+    This plug stores the current organisation id and the
+    user's permissions in the current organisation in the user struct
     based on what is found in the claims.
   """
-  import Plug.Conn
+  import Ecto.Query
   import Guardian.Plug
+  import Plug.Conn
 
+  alias WraftDoc.Account.Role
   alias WraftDoc.Enterprise.Organisation
   alias WraftDoc.Repo
 
@@ -21,9 +24,29 @@ defmodule WraftDocWeb.CurrentOrganisation do
         AuthErrorHandler.auth_error(conn, {:error, :no_org})
 
       %Organisation{} ->
-        user = conn.assigns[:current_user]
-        user = Map.put(user, :current_org_id, org_id)
+        roles_preload_query = from(r in Role, where: r.organisation_id == ^org_id)
+        user = Repo.preload(conn.assigns[:current_user], roles: roles_preload_query)
+
+        %{names: role_names, permissions: permissions} =
+          Enum.reduce(user.roles, %{names: [], permissions: []}, fn role, acc ->
+            add_role_names_and_permissions(role, acc)
+          end)
+
+        user =
+          Map.merge(user, %{
+            current_org_id: org_id,
+            role_names: role_names,
+            permissions: permissions
+          })
+
         assign(conn, :current_user, user)
     end
+  end
+
+  # Private
+  defp add_role_names_and_permissions(role, roles_acc) do
+    permissions = roles_acc.permissions |> Kernel.++(role.permissions) |> Enum.uniq()
+    names = [role.name | roles_acc.names]
+    %{names: names, permissions: permissions}
   end
 end
