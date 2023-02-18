7b2d308a3b75870230325cdb695d77de42d2c48b
Shijith K <shijithjayan@gmail.com>
2023-02-18 04:32:29 +0000

    feat: Added new authorization plug logic

--- lib/wraft_doc_web/plug/authorized.ex
+++ lib/wraft_doc_web/plug/authorized.ex
@@ -1,11 +1,45 @@
 defmodule WraftDocWeb.Plug.Authorized do
-  @moduledoc false
-  # TODO WD-364 *Refactor the existing access control plug to follow the new implementation
+  @moduledoc """
+  Checks if the user has permissions to perform an action.
+  If the user has superadmin role, we assume they have access to everything.
+  If the action doesnt have a permission listed in the controller, we assume
+  every user has acces to it.
+  """
+  import Plug.Conn
+  alias WraftDoc.Account.User
+  @superadmin_role "superadmin"
 
-  def init(_params) do
+  def init(opts), do: opts
+
+  def call(%Plug.Conn{private: %{phoenix_action: action}} = conn, opts) do
+    case Keyword.get(opts, action) do
+      nil ->
+        conn
+
+      required_permission ->
+        %User{permissions: permissions, role_names: role_names} = conn.assigns.current_user
+
+        if @superadmin_role in role_names do
+          conn
+        else
+          check_permission(conn, required_permission, permissions)
+        end
+    end
   end
 
-  def call(conn, _params) do
-    conn
+  # Private
+  defp check_permission(conn, required_permission, users_permissions) do
+    case required_permission in users_permissions do
+      true ->
+        conn
+
+      false ->
+        body = Jason.encode!(%{errors: "Unauthorized access.!"})
+
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(401, body)
+        |> halt()
+    end
   end
 end
