11f6901561527a71d6c7d7b142f6ef2ee5db4b9c
Vasil AR <vasil.abdulrasaque@aurut.com>
2023-04-14 08:16:34 +0000

    [WD-461] waiting list approval confirmed email wraft-docs/wraft-docs-api!54
    * fix: minor fix
    
    * fix:url encode waiting list approval url
    
    * fix: minor changes
    
    * fix: minor bug fix
    
    * fix: minor changes
    
    * feat: waiting list approval confirmed email

--- lib/wraft_doc_web/admin/waiting_list_admin.ex
+++ lib/wraft_doc_web/admin/waiting_list_admin.ex
@@ -3,11 +3,12 @@ defmodule WraftDocWeb.WaitingListAdmin do
   Admin  Panel for waiting list.
   """
   alias WraftDoc.WaitingLists.WaitingList
+  alias WraftDoc.Workers.EmailWorker
 
   def index(_) do
     [
       first_name: %{name: "First Name", value: fn x -> x.first_name end},
-      last_name: %{name: "Last Name", value: fn x -> x.first_name end},
+      last_name: %{name: "Last Name", value: fn x -> x.last_name end},
       email: %{name: "Email", value: fn x -> x.email end},
       status: %{name: "Status", value: fn x -> x.status end}
     ]
@@ -22,9 +23,23 @@ defmodule WraftDocWeb.WaitingListAdmin do
     ]
   end
 
-  def after_update(_conn, %WaitingList{status: :approved, email: email} = waiting_list) do
+  def after_update(
+        _conn,
+        %WaitingList{
+          status: :approved,
+          email: email,
+          first_name: first_name,
+          last_name: last_name
+        } = waiting_list
+      ) do
+    # Flag Enable
     FunWithFlags.enable(:waiting_list_registration_control, for_actor: %{email: email})
     FunWithFlags.enable(:waiting_list_organisation_create_control, for_actor: %{email: email})
+    # Send email notification
+    %{name: "#{first_name} #{last_name}", email: email}
+    |> EmailWorker.new(queue: "mailer", tags: ["waiting_list_acceptance"])
+    |> Oban.insert()
+
     {:ok, waiting_list}
   end
 
