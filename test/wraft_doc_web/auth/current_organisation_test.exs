29c59fbe4137a9d02b9a8624e82c4f73ba7edc69
Vasil AR <vasil.abdulrasaque@aurut.com>
2023-05-09 04:55:51 +0000

    [WD-440] roles and permissions check for current_organisation tests wraft-docs/wraft-docs-api!76
    * test: roles and permissions check for current_organisation tests

--- test/wraft_doc_web/auth/current_organisation_test.exs
+++ test/wraft_doc_web/auth/current_organisation_test.exs
@@ -11,43 +11,42 @@ defmodule WraftDocWeb.Auth.CurrentOrganisationTest do
   alias WraftDocWeb.Guardian
   alias WraftDocWeb.Guardian.AuthErrorHandler
 
-  test "assigns `current_organisation_id` in `current_user` in conn", %{conn: conn} do
+  test "assigns `current_org_id`,`role_names` and `permissions` in `current_user` in conn" do
     user = insert(:user_with_organisation)
+    permissions = ["layout:index", "layout:show", "layout:create", "layout:update"]
+
+    role =
+      insert(:role,
+        name: "custom role",
+        permissions: permissions,
+        organisation: List.first(user.owned_organisations)
+      )
+
+    insert(:user_role, user: user, role: role)
 
     {:ok, token, _claims} =
       Guardian.encode_and_sign(user, %{organisation_id: user.current_org_id})
 
-    opts = [module: WraftDocWeb.Guardian, error_handler: AuthErrorHandler]
-
     conn =
-      conn
-      |> put_req_header("authorization", token)
-      |> VerifyHeader.call(opts)
-      |> EnsureAuthenticated.call(opts)
-      |> LoadResource.call(opts)
-      |> CurrentUser.call([])
+      token
+      |> conn_init()
       |> CurrentOrganisation.call([])
 
     assert conn.assigns[:current_user].current_org_id == user.current_org_id
+    assert conn.assigns[:current_user].permissions == permissions
+    assert conn.assigns[:current_user].role_names == [role.name]
     refute conn.halted
   end
 
-  test "return 404 if the organisation does not exist", %{conn: conn} do
+  test "return 404 if the organisation does not exist" do
     user = insert(:user)
 
     {:ok, token, _claims} =
       Guardian.encode_and_sign(user, %{organisation_id: Ecto.UUID.generate()})
 
-    opts = [module: WraftDocWeb.Guardian, error_handler: AuthErrorHandler]
-
     conn =
-      conn
-      |> put_req_header("authorization", token)
-      |> put_resp_content_type("application/json")
-      |> VerifyHeader.call(opts)
-      |> EnsureAuthenticated.call(opts)
-      |> LoadResource.call(opts)
-      |> CurrentUser.call([])
+      token
+      |> conn_init()
       |> CurrentOrganisation.call([])
 
     assert conn.assigns[:current_user].current_org_id == nil
@@ -55,5 +54,16 @@ defmodule WraftDocWeb.Auth.CurrentOrganisationTest do
     assert conn.halted
   end
 
-  # TODO - Add tests to ensure role_names and permissions are added to current_user
+  # Private
+  defp conn_init(token) do
+    opts = [module: WraftDocWeb.Guardian, error_handler: AuthErrorHandler]
+
+    build_conn()
+    |> put_req_header("authorization", token)
+    |> put_resp_content_type("application/json")
+    |> VerifyHeader.call(opts)
+    |> EnsureAuthenticated.call(opts)
+    |> LoadResource.call(opts)
+    |> CurrentUser.call([])
+  end
 end
