f34b7f50fbac1a71cdc713e36bf21e5b57c1eafc
Vasil <vasilabdulrazak9@gmail.com>
2023-12-15 09:32:07 +0000

    [WD-712] Updated content type field delete API to delete based content type id and field id wraft-docs/wraft-docs-api!168
    * Revert "fix: minor fixes"
    
    This reverts commit 737f3ec0e539d9b576510679bcc27a019410e6d5.
    
    * fix: minor fixes
    
    * fix: updated content type field delete API to delete based content type id and field id

--- lib/wraft_doc_web/controllers/content_type_field_controller.ex
+++ lib/wraft_doc_web/controllers/content_type_field_controller.ex
@@ -15,12 +15,13 @@ defmodule WraftDocWeb.Api.V1.ContentTypeFieldController do
   Delete a Content Type Field.
   """
   swagger_path :delete do
-    PhoenixSwagger.Path.delete("/content_type_fields/{id}")
+    PhoenixSwagger.Path.delete("/content_type/{content_type_id}/field/{field_id}")
     summary("Delete a Content Type Field")
     description("API to delete a content type field")
 
     parameters do
-      id(:path, :string, "content type field id", required: true)
+      id(:path, :string, "content type id", required: true)
+      field_id(:path, :string, "content type field id", required: true)
     end
 
     response(200, "Ok", Schema.ref(:ShowContentType))
@@ -30,13 +31,12 @@ defmodule WraftDocWeb.Api.V1.ContentTypeFieldController do
   end
 
   @spec delete(Plug.Conn.t(), map) :: Plug.Conn.t()
-  def delete(conn, %{"id" => id}) do
+  def delete(conn, %{"content_type_id" => content_type_id, "field_id" => _field_id} = params) do
     current_user = conn.assigns[:current_user]
 
-    with %ContentTypeField{content_type_id: content_type_id} = content_type_field <-
-           Document.get_content_type_field(id, current_user),
+    with %ContentTypeField{} = content_type_field <- Document.get_content_type_field(params),
          :ok <- Document.delete_content_type_field(content_type_field),
-         %ContentType{} = content_type <- Document.get_content_type_from_id(content_type_id) do
+         %ContentType{} = content_type <- Document.get_content_type(current_user, content_type_id) do
       conn
       |> put_view(WraftDocWeb.Api.V1.ContentTypeView)
       |> render("show.json", content_type: content_type)
