f57362e7928a84b528393002dbaef767ab8cd0e7
Vasil <vasilabdulrazak9@gmail.com>
2024-01-18 16:04:11 +0530

    fix: minor fixes

--- lib/wraft_doc/user/user.ex
+++ lib/wraft_doc/user/user.ex
@@ -31,7 +31,7 @@ defmodule WraftDoc.Account.User do
     has_many(:states, WraftDoc.Enterprise.Flow.State, foreign_key: :creator_id)
     has_many(:data_templates, WraftDoc.Document.DataTemplate, foreign_key: :creator_id)
     has_many(:assets, WraftDoc.Document.Asset, foreign_key: :creator_id)
-    has_many(:template_assets, WraftDoc.Document.TemplateAsset, foreign_key: :creator_id)
+    has_many(:template_assets, WraftDoc.TemplateAssets.TemplateAsset, foreign_key: :creator_id)
     has_many(:build_histories, WraftDoc.Document.Instance.History, foreign_key: :creator_id)
 
     has_many(:blocks, WraftDoc.Document.Block, foreign_key: :creator_id)
