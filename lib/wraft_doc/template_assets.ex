f3dbd6f659438fa5ba4238a4f0a17f42f3c65d38
Vasil <vasil.abdulrasaque@aurut.com>
2024-01-18 16:04:11 +0530

    fix: minor fixes

--- lib/wraft_doc/template_assets.ex
+++ lib/wraft_doc/template_assets.ex
@@ -14,7 +14,7 @@ defmodule WraftDoc.TemplateAssets do
   Create a template asset.
   """
   # TODO - write test
-  @spec create_template_asset(User.t(), map) ::
+  @spec create_template_asset(User.t(), map()) ::
           {:ok, TemplateAsset.t()} | {:error, Ecto.Changset.t()}
   def create_template_asset(%{current_org_id: org_id} = current_user, params) do
     params = Map.merge(params, %{"organisation_id" => org_id})
@@ -41,7 +41,7 @@ defmodule WraftDoc.TemplateAssets do
   Index of all template assets in an organisation.
   """
   # TODO - Write tests
-  @spec template_asset_index(User.t(), map) :: map
+  @spec template_asset_index(User.t(), map()) :: map()
   def template_asset_index(%{current_org_id: organisation_id}, params) do
     query =
       from(a in TemplateAsset,
@@ -58,33 +58,26 @@ defmodule WraftDoc.TemplateAssets do
   Show a template asset.
   """
   # TODO - write tests
-  @spec show_template_asset(binary, User.t()) :: TemplateAsset.t() | {:error, atom}
+  @spec show_template_asset(binary, User.t()) :: TemplateAsset.t() | {:error, atom()}
   def show_template_asset(<<_::288>> = template_asset_id, user) do
     template_asset_id
     |> get_template_asset(user)
-    |> case do
-      %TemplateAsset{} = template_asset -> Repo.preload(template_asset, [:creator])
-      _ -> {:error, :invalid_id}
-    end
+    |> Repo.preload([:creator])
   end
 
   @doc """
   Get a template asset from its UUID.
   """
   # TODO - Write tests
-  @spec get_template_asset(binary, User.t()) :: TemplateAsset.t() | {:error, atom}
-  def get_template_asset(<<_::288>> = id, %{current_org_id: org_id}) do
-    case Repo.get_by(TemplateAsset, id: id, organisation_id: org_id) do
-      %TemplateAsset{} = template_asset -> template_asset
-      _ -> {:error, :invalid_id}
-    end
-  end
+  @spec get_template_asset(binary, User.t()) :: TemplateAsset.t() | {:error, atom()}
+  def get_template_asset(<<_::288>> = id, %{current_org_id: org_id}),
+    do: Repo.get_by(TemplateAsset, id: id, organisation_id: org_id)
 
   @doc """
   Update a template asset.
   """
   # TODO - Write tests
-  @spec update_template_asset(TemplateAsset.t(), map) ::
+  @spec update_template_asset(TemplateAsset.t(), map()) ::
           {:ok, TemplateAsset.t()} | {:error, Ecto.Changset.t()}
   def update_template_asset(template_asset, params) do
     template_asset |> TemplateAsset.update_changeset(params) |> Repo.update()
