ddd20412b38e1894102383c8abc5496534f7a980
shijithk <shijith@functionary.co>
2024-07-23 12:18:54 +0000

    refactor: Set JSON content type for auth error responses and added custom message for invalid/expired token (#60)
    - Added 'application/json' content type to all auth error responses to ensure clients properly interpret error messages. This change improves consistency and clarity in API communications. Specifically addressed cases for invalid tokens, no user found, and no organization found.
    
    Co-authored-by: Shijith K <shijithjayan@gmail.com>
    Reviewed-on: https://bucket.functionary.co/Wraft/wraft-docs-api/pulls/60

--- lib/wraft_doc_web/auth/auth_error_handler.ex
+++ lib/wraft_doc_web/auth/auth_error_handler.ex
@@ -4,20 +4,25 @@ defmodule WraftDocWeb.Guardian.AuthErrorHandler do
   """
   import Plug.Conn
 
+  def auth_error(conn, {:invalid_token, _reason}, _opts) do
+    body = Jason.encode!(%{message: "Token is either expired or invalid. Login again."})
+    conn |> put_resp_content_type("application/json") |> send_resp(401, body)
+  end
+
   def auth_error(conn, {type, _reason}, _opts) do
     body = Jason.encode!(%{message: to_string(type)})
-    send_resp(conn, 401, body)
+    conn |> put_resp_content_type("application/json") |> send_resp(401, body)
   end
 
   def auth_error(conn, {:error, :no_user}) do
     body = Jason.encode!(%{errors: "No user found"})
 
-    conn |> send_resp(404, body) |> halt()
+    conn |> put_resp_content_type("application/json") |> send_resp(404, body) |> halt()
   end
 
   def auth_error(conn, {:error, :no_org}) do
     body = Jason.encode!(%{errors: "No organisation found"})
 
-    conn |> send_resp(404, body) |> halt()
+    conn |> put_resp_content_type("application/json") |> send_resp(404, body) |> halt()
   end
 end
