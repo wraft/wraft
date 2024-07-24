1e8b46d418e173bad07cc142b67590c98c181255
Salsabeel K <salsabeel@functionary.co>
2024-07-24 17:16:14 +0000

    feat: Implement MJML for responsive email templates wraft-docs/wraft-docs-api!184
    * chore: change logo
    
    * feat: Implement MJML for responsive email templates
    
    - Introduced MJML (Mailjet Markup Language) to streamline the creation of responsive email templates.
    - Refactored existing HTML email templates to MJML for improved maintainability and consistency across various email clients.
    - Added comprehensive documentation on the usage of MJML for new email templates.
    - Ensured backward compatibility with previous email designs during the transition phase.
    - Conducted thorough testing across major email clients to ensure consistent rendering.

---
+++ lib/wraft_doc_web/mjml/join.ex
@@ -0,0 +1,3 @@
+defmodule WraftDocWeb.MJML.Join do
+  use MjmlEEx, mjml_template: "templates/join.mjml.eex"
+end
---
+++ lib/wraft_doc_web/mjml/password_reset.ex
@@ -0,0 +1,3 @@
+defmodule WraftDocWeb.MJML.PasswordReset do
+  use MjmlEEx, mjml_template: "templates/password_reset.mjml.eex"
+end
