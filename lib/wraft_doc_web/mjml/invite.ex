dc288eeecf31681725b868292b9c37565bdcd8f3
Vasil <vasil.abdulrasaque@aurut.com>
2024-07-29 16:15:06 +0530

    fix: add filter in listing pending approval to ensure that the approval status true cases are not shown or listed in pending approvals

--- lib/wraft_doc_web/mjml/invite.ex
+++ lib/wraft_doc_web/mjml/invite.ex
@@ -1,3 +1,4 @@
 defmodule WraftDocWeb.MJML.Invite do
+  @moduledoc false
   use MjmlEEx, mjml_template: "templates/invite.mjml.eex"
 end
--- lib/wraft_doc_web/mjml/join.ex
+++ lib/wraft_doc_web/mjml/join.ex
@@ -1,3 +1,4 @@
 defmodule WraftDocWeb.MJML.Join do
+  @moduledoc false
   use MjmlEEx, mjml_template: "templates/join.mjml.eex"
 end
