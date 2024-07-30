4dd7e9931e8989623d88a79a417bd736c423f8c3
Vasil <vasilabdulrazak9@gmail.com>
2024-07-30 12:09:55 +0000

    [WD-885] make-the-dashboard-ui-count-of-documents-functional-materialized-views (#62)
    ### Features and Bug Fixes
    
    This pull request includes the following changes:
    
    * **Dashboard Features:**
      * Implemented a basic structure for the dashboard stats API.
      * Optimized dashboard state updates by utilizing PostgreSQL materialized views.
      * Enhanced dashboard functionality to display total, daily, and pending approval document counts.
    * **Refresh Interval:**
      * Adjusted the refresh interval to 30 minutes.
    * **Bug Fixes:**
      * Corrected a minor issue in the user password reset Swagger API documentation.
    
    These enhancements aim to improve dashboard performance, data accuracy, and user experience.
    
    Co-authored-by: Vasil <vasil.abdulrasaque@aurut.com>
    Reviewed-on: https://bucket.functionary.co/Wraft/wraft-docs-api/pulls/62

---
+++ lib/wraft_doc_web/controllers/dashboard_controller.ex
@@ -0,0 +1,60 @@
+defmodule WraftDocWeb.Api.V1.DashboardController do
+  use WraftDocWeb, :controller
+  use PhoenixSwagger
+
+  plug WraftDocWeb.Plug.AddActionLog
+
+  plug WraftDocWeb.Plug.Authorized,
+    dashboard_stats: "dashboard:show"
+
+  action_fallback(WraftDocWeb.FallbackController)
+
+  alias WraftDoc.Document
+
+  def swagger_definitions do
+    %{
+      DashboardStatsResponse:
+        swagger_schema do
+          title("Dashboard stats")
+          description("Dashboard stats")
+
+          properties do
+            total_documents(:integer, "Total number of documents")
+            today_documents(:integer, "Total number of documents today")
+            pending_approvals(:integer, "Total number of pending approvals")
+          end
+
+          example(%{
+            total_documents: 50,
+            today_documents: 10,
+            pending_approvals: 5
+          })
+        end
+    }
+  end
+
+  @doc """
+  Get dashboard stats
+  """
+  swagger_path :dashboard_stats do
+    get("/dashboard_stats")
+    summary("Get dashboard stats")
+
+    description(
+      "Get dashboard stats , e.g. total number of documents, daily documents or pending approvals"
+    )
+
+    response(200, "OK", Schema.ref(:DashboardStatsResponse))
+    response(422, "Unprocessable Entity", Schema.ref(:Error))
+    response(401, "Unauthorized", Schema.ref(:Error))
+  end
+
+  @spec dashboard_stats(Plug.Conn.t(), map) :: Plug.Conn.t()
+  def dashboard_stats(conn, _params) do
+    current_user = conn.assigns.current_user
+
+    with stats <- Document.get_dashboard_stats(current_user) do
+      render(conn, "dashboard_stats.json", stats: stats)
+    end
+  end
+end
