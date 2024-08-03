30e411da2aa9b47bd5b80cb21aa7fac7bd82c802
Salsabeel <k.salsabeel@gmail.com>
2024-08-03 13:53:28 +0530

    chore: add flow state in approval history

--- lib/wraft_doc_web/views/instance_approval_view.ex
+++ lib/wraft_doc_web/views/instance_approval_view.ex
@@ -1,7 +1,10 @@
 defmodule WraftDocWeb.Api.V1.InstanceApprovalView do
   use WraftDocWeb, :view
 
-  alias WraftDocWeb.Api.V1.UserView
+  alias WraftDocWeb.Api.V1.{
+    StateView,
+    UserView,
+  }
 
   def render("approval_history.json", %{history: history}) do
     render_many(history, __MODULE__, "approval.json", as: :document)
@@ -10,8 +13,9 @@ defmodule WraftDocWeb.Api.V1.InstanceApprovalView do
   def render("approval.json", %{document: document}) do
     %{
       id: document.id,
-      status: document.review_status,
+      review_status: document.review_status,
       approver: render_one(document.reviewer, UserView, "user_id_and_name.json", as: :user),
+      to_state: render_one(document.to_state, StateView, "state.json", as: :state),
       reviewed_at: document.reviewed_at,
     }
   end
