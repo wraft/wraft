2eb3fd4c91977239b2a8d4d1c0eb93e20431775c
Salsabeel <k.salsabeel@gmail.com>
2024-09-25 11:46:34 +0530

    chore: add config

---
+++ config/test.exs
@@ -0,0 +1,36 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :wraft_doc, WraftDocWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :wraft_doc, WraftDoc.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("POSTGRES_USER") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+  database: System.get_env("POSTGRES_DB") || "wraft_doc_test",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+config :wraft_doc, Oban, queues: false, plugins: false
+
+config :wraft_doc, WraftDocWeb.Mailer, adapter: Swoosh.Adapters.Test
+
+config :wraft_doc, permissions_file: "test/mix/tasks/csv/test_permissions.csv"
+
+config :wraft_doc, :test_module, minio: ExAwsMock, razorpay: WraftDoc.Client.RazorpayMock
+
+config :tesla, adapter: Tesla.Mock
+
+config :waffle, storage: Waffle.Storage.Local
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SECRET_KEY_BASE=
+DATABASE_URL=
+PORT=4000
+echo " Building Image with  ${DATABASE_URL} \ ${SECRET_KEY_BASE}"
+
+docker build \
+--build-arg SECRET_KEY_BASE=${SECRET_KEY_BASE} \
+--build-arg DATABASE_URL=${DATABASE_URL} \
+--build-arg PORT=${PORT} \
+--build-arg RELEASE_NAME=wraft_doc \
+ -t wraft-docs:latest .
