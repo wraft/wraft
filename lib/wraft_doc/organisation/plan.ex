df8784a10c2bacf092bcbf3d2f50cbe20018db8f
faheemvc <faheemtyd@gmail.com>
2024-11-05 18:55:49 +0530

    feat(wraft/billing): add limits to plan and update plan

--- lib/wraft_doc/organisation/plan.ex
+++ lib/wraft_doc/organisation/plan.ex
@@ -3,23 +3,39 @@ defmodule WraftDoc.Enterprise.Plan do
   The plan model.
   """
   use WraftDoc.Schema
+
+  # alias WraftDoc.Billing.Subscription
   alias __MODULE__
+  alias __MODULE__.Limits
 
   schema "plan" do
     field(:name, :string)
     field(:description, :string)
-    field(:yearly_amount, :integer, default: 0)
-    field(:monthly_amount, :integer, default: 0)
+    field(:yearly_product_id, :string)
+    field(:yearly_amount, :string)
+    field(:monthly_product_id, :string)
+    field(:monthly_amount, :string)
+
+    embeds_one(:limits, Limits)
+
+    # has_many(:subscriptions, Subscription)
 
     timestamps()
-
-    has_many(:memberships, WraftDoc.Enterprise.Membership)
   end
 
   def changeset(%Plan{} = plan, attrs \\ %{}) do
     plan
-    |> cast(attrs, [:name, :description, :yearly_amount, :monthly_amount])
-    |> validate_required([:name, :description])
+    |> cast(attrs, [
+      :name,
+      :description,
+      :yearly_amount,
+      :monthly_amount,
+      :yearly_product_id,
+      :monthly_product_id,
+      :limits,
+      :subscriptions
+    ])
+    |> validate_required([:name, :description, :yearly_product_id, :monthly_product_id, :limits])
     |> unique_constraint(:name,
       name: :plan_unique_index,
       message: "A plan with the same name exists.!"
