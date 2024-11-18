bc0ee402c46e4a255f8ec38c344bab1521d32e8c
Sad-lie <voxsalie@gmail.com>
2024-11-18 13:19:57 +0530

    feature/typesense , refactor the typesense module

--- lib/wraft_doc/search_engine/typesense_operations.ex
+++ lib/wraft_doc/search_engine/typesense_operations.ex
@@ -1,16 +1,16 @@
-defmodule WraftDoc.SearchEngine.Indexer do
+defmodule WraftDoc.SearchEngine.TypesenseOperations do
   @moduledoc """
   Handles operations for Typesense
   """
 
   alias WraftDoc.Repo
-  alias WraftDoc.SearchEngine.Index
+  alias WraftDoc.SearchEngine.TypesenseIndex
 
   def create_document(attrs, schema) do
     with {:ok, document} <- create_repo_document(attrs, schema),
          {:ok, _response} <- ExTypesense.create_document(
-           Index.collection_name(document),
-           Index.to_document(document)
+          TypesenseIndex.collection_name(document),
+          TypesenseIndex.to_document(document)
          ) do
       {:ok, document}
     else
@@ -18,18 +18,17 @@ defmodule WraftDoc.SearchEngine.Indexer do
     end
   end
 
-
   def get_document(id, schema) do
     struct = struct(schema)
-    ExTypesense.get_document(Index.collection_name(struct), to_string(id))
+    ExTypesense.get_document(TypesenseIndex.collection_name(struct), to_string(id))
   end
 
   def update_document(id, attrs, schema) do
     with {:ok, document} <- get_repo_document(id, schema),
          {:ok, updated_document} <- update_repo_document(document, attrs, schema),
          {:ok, _response} <- ExTypesense.upsert_document(
-           Index.collection_name(updated_document),
-           Index.to_document(updated_document)
+          TypesenseIndex.collection_name(updated_document),
+          TypesenseIndex.to_document(updated_document)
          ) do
       {:ok, updated_document}
     else
@@ -41,7 +40,7 @@ defmodule WraftDoc.SearchEngine.Indexer do
     with {:ok, document} <- get_repo_document(id, schema),
          {:ok, _} <- Repo.delete(document),
          {:ok, _response} <- ExTypesense.delete_document(
-           Index.collection_name(document),
+          TypesenseIndex.collection_name(document),
            to_string(id)
          ) do
       {:ok, document}
@@ -52,7 +51,7 @@ defmodule WraftDoc.SearchEngine.Indexer do
 
   def search_documents(query, schema, opts \\ []) do
     struct = struct(schema)
-    collection_name = Index.collection_name(struct)
+    collection_name = TypesenseIndex.collection_name(struct)
 
     search_params = %{
       q: query,
@@ -63,29 +62,12 @@ defmodule WraftDoc.SearchEngine.Indexer do
 
     ExTypesense.search(collection_name, search_params)
   end
-  # def search(query, options \\ %{}) do
-  #   search_parameters = Map.merge(%{
-  #     q: query,
-  #     query_by: "name,description",
-  #     sort_by: "inserted_at:desc"
-  #   }, options)
-
-  #   case Typesense.collections("content_types").documents().search(search_parameters) do
-  #     {:ok, results} -> {:ok, process_results(results)}
-  #     {:error, error} -> {:error, error}
-  #   end
-  # end
-
-  # defp process_results(results) do
-  #   # Process and format the search results as needed
-  #   results
-  # end
 
   def bulk_index_documents(documents) when is_list(documents) do
     case documents do
       [first | _] ->
-        collection_name = Index.collection_name(first)
-        documents_map = Enum.map(documents, &Index.to_document/1)
+        collection_name = TypesenseIndex.collection_name(first)
+        documents_map = Enum.map(documents, &TypesenseIndex.to_document/1)
         ExTypesense.import_documents(collection_name, documents_map)
 
       [] ->
@@ -95,12 +77,16 @@ defmodule WraftDoc.SearchEngine.Indexer do
 
   def setup_collection(schema) do
     struct = struct(schema)
-    collection_name = Index.collection_name(struct)
-    collection_schema = Index.collection_schema(struct)
+    collection_name = TypesenseIndex.collection_name(struct)
+    collection_schema = TypesenseIndex.collection_schema(struct)
 
-    # First try to delete existing collection (ignore if it doesn't exist)
-    _ = ExTypesense.delete_collection(collection_name)
+    # First try to delete existing collection
+    case ExTypesense.get_collection(collection_name) do
+      {:ok, _} -> ExTypesense.delete_collection_alias(collection_name)
+      _ -> :ok
+    end
 
+    # Create new collection
     ExTypesense.create_collection(collection_schema)
   end
 
@@ -126,10 +112,23 @@ defmodule WraftDoc.SearchEngine.Indexer do
 
   defp search_fields_from_schema(struct) do
     struct
-    |> Index.collection_schema()
+    |> TypesenseIndex.collection_schema()
     |> Map.get(:fields)
     |> Enum.filter(&(&1.type == "string"))
     |> Enum.map(&(&1.name))
     |> Enum.reject(&(&1 in ["id", "inserted_at", "updated_at"]))
   end
+
+  # defp convert_ids_to_binary(attrs) do
+  #   attrs
+  #   |> Enum.map(fn {key, value} ->
+  #     case key do
+  #       key when key in [:layout_id, :flow_id, :theme_id, :organisation_id, :creator_id] ->
+  #         {key, Ecto.UUID.generate()}
+  #       _ ->
+  #         {key, value}
+  #     end
+  #   end)
+  #   |> Map.new()
+  # end
 end
