fff7e8ff7c076e67cf1b4263d472886de1a50605
Vasil <vasilabdulrazak9@gmail.com>
2024-11-21 16:18:00 +0530

    feat:add start date to contract clm

--- lib/wraft_doc/document/instance/contract_meta.ex
+++ lib/wraft_doc/document/instance/contract_meta.ex
@@ -5,17 +5,17 @@ defmodule WraftDoc.Document.Instance.ContractMeta do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @contract_fields ~w(type status expiry_date contract_value counter_parties clauses reminder)a
+  @contract_fields ~w(type status start_date expiry_date contract_value counter_parties reminder)a
   @contract_status ~w(draft review active expired)a
 
   @primary_key false
   embedded_schema do
     field(:type, Ecto.Enum, values: [:contract])
     field(:status, Ecto.Enum, values: @contract_status, default: :draft)
+    field(:start_date, :date)
     field(:expiry_date, :date)
     field(:contract_value, :decimal)
     field(:counter_parties, {:array, :string}, default: [])
-    field(:clauses, {:array, :map}, default: [])
     field(:reminder, {:array, :map}, default: [])
   end
 
