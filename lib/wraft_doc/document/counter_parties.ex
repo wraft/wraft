0d8608ec03282185859f71918c595d3b8bc5249b
Vasil <vasilabdulrazak9@gmail.com>
2024-12-04 10:19:16 +0530

    feat: add schemas for counter parties

---
+++ lib/wraft_doc/document/counter_parties.ex
@@ -0,0 +1,20 @@
+defmodule WraftDoc.Document.CounterParties do
+  @moduledoc """
+    This is the Counter Parties module
+  """
+  use WraftDoc.Schema
+
+  schema "counter_parties" do
+    field(:name, :string)
+    belongs_to(:content, WraftDoc.Document.Content)
+    belongs_to(:guest_user, WraftDoc.Account.GuestUser)
+    timestamps()
+  end
+
+  def changeset(counter_parties, attrs) do
+    counter_parties
+    |> cast(attrs, [:name, :content_id, :guest_user_id])
+    |> validate_required([:name, :content_id, :guest_user_id])
+    |> unique_constraint([:content_id, :guest_user_id], message: "already exist")
+  end
+end
