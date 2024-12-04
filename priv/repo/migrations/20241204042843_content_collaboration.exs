2fa612330429ad96450ee168301f032adef87fac
Vasil <vasilabdulrazak9@gmail.com>
2024-12-04 10:04:55 +0530

    feat: add schema migration table for content collaboration

---
+++ priv/repo/migrations/20241204042843_content_collaboration.exs
@@ -0,0 +1,25 @@
+defmodule WraftDoc.Repo.Migrations.ContentCollaboration do
+  use Ecto.Migration
+
+  def change do
+    drop(table(:content_collab))
+
+    create table(:content_collaboration, primary_key: false) do
+      add(:id, :uuid, primary_key: true)
+      add(:roles, :string)
+      add(:content_id, references(:content, type: :uuid, column: :id, on_delete: :nilify_all))
+      add(:state_id, references(:state, type: :uuid, column: :id, on_delete: :nilify_all))
+      add(:user_id, references(:user, type: :uuid, column: :id, on_delete: :nilify_all))
+
+      add(
+        :guest_user_id,
+        references(:guest_user, type: :uuid, column: :id, on_delete: :nilify_all)
+      )
+
+      timestamps()
+    end
+
+    create(unique_index(:content_collaboration, [:content_id, :user_id, :state_id]))
+    create(unique_index(:content_collaboration, [:content_id, :guest_user_id, :state_id]))
+  end
+end
