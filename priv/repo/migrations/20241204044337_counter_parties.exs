91e249a608eda43b306be42ae4acf1008dfe4ad5
Vasil <vasilabdulrazak9@gmail.com>
2024-12-17 18:04:57 +0530

    fix: remove guest_user table

--- priv/repo/migrations/20241204044337_counter_parties.exs
+++ priv/repo/migrations/20241204044337_counter_parties.exs
@@ -2,18 +2,14 @@ defmodule WraftDoc.Repo.Migrations.CounterParties do
   use Ecto.Migration
 
   def change do
+    # TODO  need to improve
     create table(:counter_parties, primary_key: false) do
       add(:id, :uuid, primary_key: true)
       add(:name, :string)
 
-      add(
-        :guest_user_id,
-        references(:guest_user, type: :uuid, column: :id, on_delete: :nilify_all)
-      )
-
       add(:content_id, references(:content, type: :uuid, column: :id, on_delete: :nilify_all))
     end
 
-    create(unique_index(:counter_parties, [:content_id, :guest_user_id]))
+    create(unique_index(:counter_parties, [:content_id]))
   end
 end
