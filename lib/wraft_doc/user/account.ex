63c56993d0eb4804a24c11912a3e31ac4f38a902
Faheemvc <95545303+Faheemvc@users.noreply.github.com>
2025-03-28 18:22:48 +0530

    fix: remove redundant subcription (#68)

--- lib/wraft_doc/user/account.ex
+++ lib/wraft_doc/user/account.ex
@@ -137,20 +137,6 @@ defmodule WraftDoc.Account do
         "personal_organisation_roles"
       )
     end)
-    |> then(fn multi ->
-      if Enterprise.self_hosted?() do
-        multi
-      else
-        Multi.run(multi, :free_subscription, fn _repo,
-                                                %{
-                                                  personal_organisation: %{
-                                                    organisation: organisation
-                                                  }
-                                                } ->
-          Enterprise.create_free_subscription(organisation.id)
-        end)
-      end
-    end)
     |> Multi.run(:default_flow, fn _repo,
                                    %{
                                      user: user,
