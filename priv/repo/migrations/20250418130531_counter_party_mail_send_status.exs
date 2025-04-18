e09018b79698fba8788e81e9154f5b6381417118
Vasil <vasilabdulrazak9@gmail.com>
2025-04-18 22:05:09 +0530

    fix: minor fix

--- priv/repo/migrations/20250418130531_counter_party_mail_send_status.exs
+++
@@ -1,9 +0,0 @@
-defmodule WraftDoc.Repo.Migrations.CounterPartyMailSendStatus do
-  use Ecto.Migration
-
-  def change do
-    alter table(:counter_parties) do
-      add(:mail_send_status, :boolean, null: false, default: false)
-    end
-  end
-end
