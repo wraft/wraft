cbd757c5a18c5ef52753997f92b93737ef090c5a
Faheem vc <faheemtyd@gmail.com>
2025-06-03 15:04:13 +0530

    chore: add swagger documention to health api

--- lib/wraft_doc_web/controllers/health_controller.ex
+++ lib/wraft_doc_web/controllers/health_controller.ex
@@ -1,11 +1,35 @@
 defmodule WraftDocWeb.Api.V1.HealthController do
   @moduledoc """
-  Controller module for Instance approval
+  Simple health check API that always responds with status `ok`.
   """
   use WraftDocWeb, :controller
   use PhoenixSwagger
 
-  def check_health(conn, _) do
-    json(conn, %{status: "ok"})
+  def swagger_definitions do
+    %{
+      HealthResponse:
+        swagger_schema do
+          title("HealthResponse")
+          description("Health check response")
+
+          properties do
+            status(:string, "Service status", example: "ok")
+          end
+
+          example(%{
+            status: "ok"
+          })
+        end
+    }
   end
+
+  swagger_path :check_health do
+    get("/health")
+    summary("Service Health Check")
+    description("Returns a simple `ok` status if the service is up.")
+
+    response(200, "Successful response", Schema.ref(:HealthResponse))
+  end
+
+  def check_health(conn, _), do: json(conn, %{status: "ok"})
 end
