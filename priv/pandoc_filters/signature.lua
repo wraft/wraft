a4c0e1179b3fe04098bfa6d1f651fa8c053d3fd5
Faheem vc <faheemtyd@gmail.com>
2025-07-04 18:50:46 +0530

    fix: issue getting toc links as the signature box in latex

--- priv/pandoc_filters/signature.lua
+++ priv/pandoc_filters/signature.lua
@@ -1,27 +1,27 @@
 local function px_to_cm(px)
-  return string.format("%.2f", px * 2.54 / 96)
+    return string.format("%.2f", px * 2.54 / 96)
 end
 
 local function px_to_pt(px)
-  return string.format("%.2f", px * 0.75)
+    return string.format("%.2f", px * 0.75)
 end
 
 function Para(el)
-  local text = pandoc.utils.stringify(el)
+    local text = pandoc.utils.stringify(el)
 
-  if text:match("%[SIGNATURE_FIELD_PLACEHOLDER") then
-    local width = tonumber(text:match("width:(%d+)"))
-    local height = tonumber(text:match("height:(%d+)"))
-    if not width or not height then return el end
+    if text:match("%[SIGNATURE_FIELD_PLACEHOLDER") then
+        local width = tonumber(text:match("width:(%d+)"))
+        local height = tonumber(text:match("height:(%d+)"))
+        if not width or not height then return el end
 
-    local width_cm = px_to_cm(width)
-    local height_cm = px_to_cm(height)
+        local width_cm = px_to_cm(width)
+        local height_cm = px_to_cm(height)
 
-    local width_pt = px_to_pt(width)
-    local height_pt = px_to_pt(height)
+        local width_pt = px_to_pt(width)
+        local height_pt = px_to_pt(height)
 
-    local latex_code = string.format("\\TextField[width=%scm,height=%scm]{}", width_cm, height_cm)
-    local typst_code = string.format([[
+        local latex_code = string.format("\\sigField[\\BC{0 0 0}]{signatureField}{%scm}{%scm}", width_cm, height_cm)
+        local typst_code = string.format([[
     #rect(
       stroke: rgb(0, 184, 148),
       width: %spt,
@@ -30,11 +30,11 @@ function Para(el)
     )
     ]], width_pt, height_pt)
 
-    return pandoc.Para({
-      pandoc.RawInline("latex", latex_code),
-      pandoc.RawInline("typst", typst_code),
-    })
-  end
+        return pandoc.Para({
+            pandoc.RawInline("latex", latex_code),
+            pandoc.RawInline("typst", typst_code),
+        })
+    end
 
-  return el
+    return el
 end
