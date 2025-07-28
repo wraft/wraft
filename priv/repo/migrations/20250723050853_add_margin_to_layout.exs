10886b5200be1609092139fddaa8e998857b9fd7
Faheem vc <faheemtyd@gmail.com>
2025-07-28 19:22:53 +0530

    feat: seed margin to layout table

--- priv/repo/migrations/20250723050853_add_margin_to_layout.exs
+++ priv/repo/migrations/20250723050853_add_margin_to_layout.exs
@@ -7,6 +7,12 @@ defmodule WraftDoc.Repo.Migrations.AddMarginToLayout do
     alter table(:layouts) do
       add(:margin, :map)
     end
+
+    execute("""
+    UPDATE layouts
+    SET margin = '{"top": 2.5, "left": 2.5, "right": 2.5, "bottom": 2.5}'
+    WHERE margin IS NULL
+    """)
   end
 
   def down do
