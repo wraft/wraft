ff17d2745bc04aebee04c6e16b83444d3cfea4b7
Faheem vc <faheemtyd@gmail.com>
2025-07-28 10:22:38 +0530

    feat: add swagger docs

--- lib/wraft_doc/themes/fonts.ex
+++ lib/wraft_doc/themes/fonts.ex
@@ -4,12 +4,23 @@ defmodule WraftDoc.Themes.Fonts do
   """
   import Ecto.Query
 
+  alias Ecto.Multi
   alias WraftDoc.Assets
   alias WraftDoc.Assets.Asset
   alias WraftDoc.Repo
   alias WraftDoc.Themes.Font
   alias WraftDoc.Themes.FontAsset
 
+  @doc """
+  Lists all fonts for the given organisation, preloading their assets.
+
+  ## Parameters
+    - current_user: a map containing at least `:current_org_id`
+
+  ## Returns
+    - List of Font structs with preloaded assets.
+  """
+  @spec list_fonts(User.t()) :: [Font.t()]
   def list_fonts(%{current_org_id: current_org_id} = _current_user) do
     Font
     |> where([f], f.organisation_id == ^current_org_id)
@@ -17,52 +28,99 @@ defmodule WraftDoc.Themes.Fonts do
     |> Repo.preload(:assets)
   end
 
+  @doc """
+  Gets a font by its ID, preloading its assets.
+
+  ## Parameters
+    - id: Font ID
+
+  ## Returns
+    - Font struct with preloaded assets, or nil if not found.
+  """
+  @spec get_font(Ecto.UUID.t()) :: Font.t() | nil
   def get_font(id) do
     Font
     |> Repo.get(id)
     |> Repo.preload(:assets)
   end
 
-  def create_font(%{id: user_id, current_org_id: current_org_id} = current_user, attrs \\ %{}) do
-    %Font{}
-    |> Font.changeset(
-      Map.merge(attrs, %{"creator_id" => user_id, "organisation_id" => current_org_id})
-    )
-    |> Repo.insert()
-    |> case do
-      {:ok, font} ->
-        fetch_and_associate_assets_with_font(font, current_user, attrs)
+  @doc """
+  Creates a font and its associated assets.
 
+  ## Parameters
+    - current_user: map with `:id` and `:current_org_id`
+    - attrs: attributes for the font, including `"files"` for assets
+
+  ## Returns
+    - {:ok, Font.t()} on success
+    - {:error, reason} on failure
+  """
+  @spec create_font(User.t(), map()) :: {:ok, Font.t()} | {:error, Ecto.Changeset.t()}
+  def create_font(%{id: user_id, current_org_id: current_org_id} = current_user, attrs \\ %{}) do
+    Multi.new()
+    |> Multi.insert(:create_font, fn _changeset ->
+      Font.changeset(
+        %Font{},
+        Map.merge(attrs, %{"creator_id" => user_id, "organisation_id" => current_org_id})
+      )
+    end)
+    |> Multi.run(:create_assets, fn _repo, _changeset ->
+      create_assets(current_user, List.wrap(attrs["files"]))
+    end)
+    |> Multi.run(:create_font_assets, fn _repo, %{create_font: font, create_assets: assets} ->
+      Enum.each(assets, &associate_font_and_asset(font, &1))
+      {:ok, nil}
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{create_font: font}} ->
         {:ok, Repo.preload(font, [:assets])}
 
-      {:error, changeset} ->
-        {:error, changeset}
+      {:error, _, error, _} ->
+        {:error, error}
     end
   end
 
-  def update_font(%Font{} = font, attrs) do
-    font
-    |> Font.changeset(attrs)
-    |> Repo.update()
+  @doc """
+  Updates a font with the given attributes.
+
+  ## Parameters
+    - font: Font struct
+    - current_user: User struct
+    - attrs: attributes to update
+
+  ## Returns
+    - {:ok, Font.t()} on success
+    - {:error, changeset} on failure
+  """
+  @spec update_font(Font.t(), User.t(), map()) :: {:ok, Font.t()} | {:error, Ecto.Changeset.t()}
+  def update_font(%Font{} = font, current_user, attrs) do
+    Multi.new()
+    |> Multi.update(:update_font, Font.changeset(font, attrs))
+    |> maybe_replace_assets(font, current_user, attrs)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{update_font: font}} ->
+        {:ok, Repo.preload(font, [:assets])}
+
+      {:error, _, error, _} ->
+        {:error, error}
+    end
   end
 
-  def delete_font(%Font{} = font) do
-    Repo.delete(font)
-  end
+  @doc """
+  Deletes the given font.
 
-  # TODO accept files directly then create assets
-  # Get all the assets from their UUIDs and associate them with the given theme.
-  defp fetch_and_associate_assets_with_font(font, current_user, %{"assets" => assets}) do
-    (assets || "")
-    |> String.split(",")
-    |> Stream.map(fn asset -> Assets.get_asset(asset, current_user) end)
-    |> Stream.map(fn asset -> associate_font_and_asset(font, asset) end)
-    |> Enum.to_list()
-  end
+  ## Parameters
+    - font: Font struct
 
-  defp fetch_and_associate_assets_with_font(_font, _current_user, _params), do: []
+  ## Returns
+    - {:ok, Font.t()} on success
+    - {:error, changeset} on failure
+  """
+  @spec delete_font(Font.t()) :: {:ok, Font.t()} | {:error, Ecto.Changeset.t()}
+  def delete_font(%Font{} = font), do: Repo.delete(font)
 
-  # Associate the asset with the given theme, ie; insert a ThemeAsset entry.
   defp associate_font_and_asset(font, %Asset{} = asset) do
     %FontAsset{}
     |> FontAsset.changeset(%{font_id: font.id, asset_id: asset.id})
@@ -70,4 +128,35 @@ defmodule WraftDoc.Themes.Fonts do
   end
 
   defp associate_font_and_asset(_font, _asset), do: nil
+
+  defp maybe_replace_assets(multi, font, current_user, %{"files" => files})
+       when is_list(files) and length(files) > 0 do
+    multi
+    |> Multi.delete_all(:delete_font_assets, Ecto.assoc(font, :font_assets))
+    |> Multi.delete_all(:delete_assets, Ecto.assoc(font, :assets))
+    |> Multi.run(:create_assets, fn _repo, _changes ->
+      create_assets(current_user, files)
+    end)
+    |> Multi.run(:create_font_assets, fn _repo, %{update_font: font, create_assets: assets} ->
+      Enum.each(assets, &associate_font_and_asset(font, &1))
+      {:ok, nil}
+    end)
+  end
+
+  defp maybe_replace_assets(multi, _font, _current_user, _attrs), do: multi
+
+  defp create_assets(current_user, files) do
+    files
+    |> Enum.map(
+      &Assets.create_asset(current_user, %{
+        "file" => &1,
+        "type" => "theme",
+        "name" => &1.filename
+      })
+    )
+    |> Enum.reduce({:ok, []}, fn
+      {:ok, asset}, {:ok, acc} -> {:ok, [asset | acc]}
+      {:error, _} = err, _ -> err
+    end)
+  end
 end
--- lib/wraft_doc_web/controllers/font_controller.ex
+++ lib/wraft_doc_web/controllers/font_controller.ex
@@ -2,7 +2,7 @@ defmodule WraftDocWeb.Api.V1.FontController do
   use WraftDocWeb, :controller
   use PhoenixSwagger
 
-  plug(WraftDocWeb.Plug.AddActionLog)
+  # plug(WraftDocWeb.Plug.AddActionLog)
 
   # plug(WraftDocWeb.Plug.Authorized,
   #   create: "form:manage",
@@ -15,19 +15,115 @@ defmodule WraftDocWeb.Api.V1.FontController do
 
   action_fallback(WraftDocWeb.FallbackController)
 
+  alias WraftDoc.Themes.Font
   alias WraftDoc.Themes.Fonts
-  # alias WraftDoc.Themes.Fonts.Font
 
+  def swagger_definitions do
+    %{
+      FontCreateParams:
+        swagger_schema do
+          title("FontCreateParams")
+          description("Parameters for creating a font")
+
+          properties do
+            name(:string, "Font name", required: true)
+            files(Schema.array(:file), "List of font files", required: true)
+          end
+
+          example(%{
+            name: "Roboto",
+            files: [%Plug.Upload{path: "uploads/fonts/roboto.ttf", filename: "roboto.ttf"}]
+          })
+        end,
+      FontUpdateParams:
+        swagger_schema do
+          title("FontUpdateParams")
+          description("Parameters for updating a font")
+
+          properties do
+            name(:string, "Font name", required: false)
+            files(Schema.array(:file), "List of font files", required: false)
+          end
+
+          example(%{
+            name: "Roboto Updated",
+            files: [%Plug.Upload{path: "uploads/fonts/roboto.ttf", filename: "roboto.ttf"}]
+          })
+        end,
+      Font:
+        swagger_schema do
+          title("Font")
+          description("A font resource")
+
+          properties do
+            id(:string, "Font ID")
+            name(:string, "Font name")
+            files(Schema.array(:file), "List of font files")
+          end
+
+          example(%{
+            id: "1",
+            name: "Roboto",
+            files: [%Plug.Upload{path: "uploads/fonts/roboto.ttf", filename: "roboto.ttf"}]
+          })
+        end
+    }
+  end
+
+  swagger_path :index do
+    get("/fonts")
+    summary("List all fonts")
+    description("Retrieves a list of all fonts for the current user.")
+
+    response(200, "OK", Schema.array(:Font))
+  end
+
+  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, _params) do
     current_user = conn.assigns.current_user
     fonts = Fonts.list_fonts(current_user)
     render(conn, "index.json", fonts: fonts)
   end
 
+  swagger_path :show do
+    get("/fonts/{id}")
+    summary("Get a font by ID")
+    description("Retrieves a specific font by its ID.")
+    produces("application/json")
+
+    parameters do
+      id(:path, :string, "Font ID", required: true)
+    end
+
+    response(200, "OK", Schema.ref(:Font))
+    response(404, "Not Found")
+  end
+
+  @spec show(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def show(conn, %{"id" => id}) do
     font = Fonts.get_font(id)
     render(conn, "font.json", font: font)
   end
+
+  swagger_path :create do
+    post("/fonts")
+    summary("Create a font")
+    description("Creates a new font with a name and a list of files.")
+    consumes("multipart/form-data")
+
+    parameter(:name, :formData, :string, "Layout's name", required: true)
+
+    parameter(:files, :formData, :file, "Font files (multiple allowed)",
+      required: true,
+      type: :file,
+      collectionFormat: "multi"
+    )
+
+    response(201, "Font created", Schema.ref(:Font))
+    response(400, "Bad request")
+  end
+
+  @spec create(Plug.Conn.t(), map()) :: Plug.Conn.t()
 
   def create(conn, params) do
     current_user = conn.assigns.current_user
@@ -36,6 +132,54 @@ defmodule WraftDocWeb.Api.V1.FontController do
       conn
       |> put_status(:created)
       |> render("font.json", font: font)
+    end
+  end
+
+  swagger_path :update do
+    put("/fonts/{id}")
+    summary("Update a font")
+    description("Updates an existing font's name and files list.")
+    consumes("multipart/form-data")
+
+    parameter(:name, :formData, :string, "Layout's name", required: true)
+
+    parameter(:files, :formData, :file, "Font files (multiple allowed)",
+      required: true,
+      type: :file,
+      collectionFormat: "multi"
+    )
+
+    response(200, "Font updated", Schema.ref(:Font))
+    response(400, "Bad request")
+  end
+
+  @spec update(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def update(conn, %{"id" => id} = params) do
+    current_user = conn.assigns.current_user
+
+    with {:ok, font} <- Fonts.update_font(current_user, id, params) do
+      render(conn, "font.json", font: font)
+    end
+  end
+
+  swagger_path :delete do
+    PhoenixSwagger.Path.delete("/fonts/{id}")
+    summary("Delete a font")
+    description("Deletes a specific font by its ID.")
+
+    parameters do
+      id(:path, :string, "Font ID", required: true)
+    end
+
+    response(204, "No Content")
+    response(404, "Not Found")
+  end
+
+  @spec delete(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def delete(conn, %{"id" => id}) do
+    with %Font{} = font <- Fonts.get_font(id),
+         %Font{} <- Fonts.delete_font(font) do
+      send_resp(conn, :no_content, "")
     end
   end
 end
