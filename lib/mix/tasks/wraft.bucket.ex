10a8022ca774d355134a1f46c16be3d902499934
Salsabeel <k.salsabeel@gmail.com>
2025-08-07 10:01:59 +0530

    feat: add wraft.bucket task to create default storage bucket and update mix aliases

---
+++ lib/mix/tasks/wraft.bucket.ex
@@ -0,0 +1,39 @@
+defmodule Mix.Tasks.Wraft.Bucket do
+  @moduledoc """
+  Creates the default storage bucket if it doesn't exist.
+
+  ## Examples
+
+      $ mix wraft.bucket
+  """
+
+  @shortdoc "Creates default storage bucket"
+
+  use Mix.Task
+  require Logger
+
+  alias WraftDoc.Minio.Utils
+
+  @requirements ["app.start"]
+  def run(_) do
+    bucket = System.get_env("MINIO_BUCKET", "wraft")
+    Logger.info("Checking if bucket '#{bucket}' exists...")
+
+    case Utils.bucket_exists?(bucket) do
+      true ->
+        Logger.info("Bucket '#{bucket}' already exists.")
+
+      false ->
+        Logger.info("Creating bucket '#{bucket}'...")
+
+        case Utils.create_bucket(bucket) do
+          {:ok, _} ->
+            Logger.info("Bucket '#{bucket}' created successfully.")
+
+          {:error, error} ->
+            Logger.error("Failed to create bucket '#{bucket}': #{inspect(error)}")
+            raise "Failed to create bucket '#{bucket}'"
+        end
+    end
+  end
+end
