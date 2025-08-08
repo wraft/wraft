12d890c8356bd6769f5499500b297863c8190815
Salsabeel <k.salsabeel@gmail.com>
2025-08-08 14:47:38 +0530

    chore: update environment configuration and Docker setup for improved dependency management and keystore handling

--- entrypoint_dev.sh
+++ entrypoint_dev.sh
@@ -26,6 +26,7 @@ echo "$(date) - [] PostgreSQL is ready"
 if [[ -z `psql -Atqc "\\list $DB_NAME"` ]]; then
   echo "Database $DB_NAME does not exist. Creating..."
   createdb -E UTF8 $DB_NAME -l en_US.UTF-8 -T template0
+  mix wraft.bucket
   mix ecto.migrate
   mix run priv/repo/seeds.exs
   echo "Database $DB_NAME created."
