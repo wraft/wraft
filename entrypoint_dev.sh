65ee0886ee71fb1079523b0033eb86dc5ab0c21b
Salsabeel <k.salsabeel@gmail.com>
2025-08-20 13:16:40 +0530

    fix: enhance database name extraction and increase wait time for readiness in entrypoint script

--- entrypoint_dev.sh
+++ entrypoint_dev.sh
@@ -13,13 +13,13 @@ DB_USER=$(echo $DATABASE_URL | sed -n 's/^postgres:\/\/\([^:]*\):.*/\1/p')
 DB_PASS=$(echo $DATABASE_URL | sed -n 's/^postgres:\/\/[^:]*:\([^@]*\)@.*/\1/p')
 DB_HOST=$(echo $DATABASE_URL | sed -n 's/^postgres:\/\/[^:]*:[^@]*@\([^:]*\):.*/\1/p')
 DB_PORT=$(echo $DATABASE_URL | sed -n 's/^postgres:\/\/[^:]*:[^@]*@[^:]*:\([^/]*\).*/\1/p')
-DB_NAME=$(echo $DATABASE_URL | sed -n 's/^postgres:\/\/[^:]*:[^@]*@[^:]*:[^/]*\/\(.*\)$/\1/p')
+DB_NAME=$(echo $DATABASE_URL | sed -n 's/^postgres:\/\/[^:]*:[^@]*@[^:]*:[^/]*\/\([^?]*\).*$/\1/p')
 
 
 while ! PGPASSWORD=$DB_PASS pg_isready -q -h $DB_HOST -p $DB_PORT -U $DB_USER
 do
   echo "$(date) - [] Waiting for Database to start"
-  sleep 2
+  sleep 5
 done
 
 echo "$(date) - [] PostgreSQL is ready"
@@ -28,6 +28,7 @@ if [[ -z `psql -Atqc "\\list $DB_NAME"` ]]; then
   echo "Database $DB_NAME does not exist. Creating..."
   createdb -E UTF8 $DB_NAME -l en_US.UTF-8 -T template0
   echo "Database $DB_NAME created."
+  sleep 2
 fi
 
 mix wraft.bucket
