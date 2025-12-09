c8d70cdb85eb1df17481b42472ca77dd0fb7a595
Faheem vc <faheemtyd@gmail.com>
2025-12-09 10:06:12 +0530

    feat: update migration file to add numerical suffix for prefix

--- priv/repo/migrations/20251208132445_update_content_type_table_update_prefix.exs
+++ priv/repo/migrations/20251208132445_update_content_type_table_update_prefix.exs
@@ -1,13 +1,35 @@
-defmodule WraftDoc.Repo.Migrations.UpdateContentTypeTableUpdatePrefix do
+defmodule WraftDoc.Repo.Migrations.FixDuplicatePrefixesWithNumericSuffix do
   use Ecto.Migration
 
   def up do
+    execute("""
+    WITH duplicates AS (
+      SELECT
+        id,
+        prefix,
+        organisation_id,
+        inserted_at,
+        ROW_NUMBER() OVER (
+          PARTITION BY organisation_id, prefix
+          ORDER BY inserted_at
+        ) AS row_num
+      FROM content_type
+    )
+    UPDATE content_type AS ct
+    SET prefix = d.prefix || (d.row_num - 1)
+    FROM duplicates d
+    WHERE ct.id = d.id
+      AND d.row_num > 1;
+    """)
+
     create(
       unique_index(:content_type, [:organisation_id, :prefix], name: :unique_org_prefix_index)
     )
   end
 
   def down do
-    drop(index(:content_type, [:organisation_id, :prefix], name: :unique_org_prefix_index))
+    drop_if_exists(
+      index(:content_type, [:organisation_id, :prefix], name: :unique_org_prefix_index)
+    )
   end
 end
