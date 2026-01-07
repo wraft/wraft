bcc3e8f01abae0266f69bb30f5ba48fb9d72f337
Salsabeel <k.salsabeel@gmail.com>
2026-01-07 17:44:43 +0530

    chore: update version to 0.6.5 and display it on the index page

--- lib/wraft_doc_web/controllers/page_controller.ex
+++ lib/wraft_doc_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule WraftDocWeb.PageController do
   use WraftDocWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    version = Application.spec(:wraft_doc, :vsn) |> to_string()
+    render(conn, "index.html", version: version)
   end
 end
